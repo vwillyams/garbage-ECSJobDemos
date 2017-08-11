d1b0a50214240e3e3e6d5dc82cc1aafa33eb4a6f
Adrian T <ahskbhsk-code@yahoo.com>
2017-08-11 13:11:14 +0200

    [ai] Added CrowdSpawner script to easily spawn many CrowdAgents.

---
+++ AI_Prototyping/LowLevelQueryAPI/Assets/CrowdSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdSpawner : MonoBehaviour
+{
+    public int newAgents = 16;
+    public float range = 11.2f;
+    public GameObject prefab;
+
+    public int totalAgentCount = 0;
+
+    void Update()
+    {
+        for (int i = 0; i < newAgents; ++i)
+        {
+            var pos = new Vector3(Random.Range(-range, range), 0, Random.Range(-range, range));
+            var go = GameObject.Instantiate(prefab, pos, Quaternion.identity, transform) as GameObject;
+            go.name = "CrowdAgent_" + (totalAgentCount + i);
+        }
+        totalAgentCount += newAgents;
+        newAgents = 0;
+    }
+}

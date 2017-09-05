5f6c215cfe8dab704c29e5d4dc34b0c6e1ec1d7a
joeante <joe@Joachims-MacBook-Pro.local>
2017-09-05 10:52:40 +0200

    Renaming some performance test scripts

--- compute-boid-simulation/Assets/ECS/InstantiatePerfTest/ECSInstantiatePerformance.cs
+++ compute-boid-simulation/Assets/ECS/InstantiatePerfTest/ECSInstantiatePerformance.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 using UnityEngine.ECS;
 using UnityEngine.Profiling;
 
-public struct IteratePerfData : IComponentData
+public class ECSInstantiatePerformance : MonoBehaviour
 {
-	public int random;
-}
-public class IteratePerfTest : MonoBehaviour
-{
-	CustomSampler iterateSampler;
-	CustomSampler getSampler;
+	CustomSampler instantiateSampler;
+	CustomSampler destroySampler;
 
 	void Awake()
 	{
-		iterateSampler = CustomSampler.Create("IterateTest");
-		getSampler = CustomSampler.Create("GetComponentDataTest");
+		instantiateSampler = CustomSampler.Create("InstantiateTest");
+		destroySampler = CustomSampler.Create("DestroyTest");
 	}
 
 	void Update()
@@ -25,38 +21,25 @@ public class IteratePerfTest : MonoBehaviour
 
 		var oldRoot = DependencyManager.Root;
 		DependencyManager.Root = new DependencyManager ();
-		DependencyManager.SetDefaultCapacity (100000 * 3);
+		DependencyManager.SetDefaultCapacity (100000 * 2);
 
 		var m_EntityManager = DependencyManager.GetBehaviourManager<EntityManager>();
 
-		var group0 = new EntityGroup (m_EntityManager, typeof(IteratePerfData));
+		var group0 = new EntityGroup (m_EntityManager, typeof(BoidSimulations.BoidData));
 		var group1 = new EntityGroup (m_EntityManager, typeof(BoidSimulations.BoidData));
 
 		var entity = m_EntityManager.AllocateEntity ();
 		m_EntityManager.AddComponent (entity, new BoidSimulations.BoidData());
-		m_EntityManager.AddComponent (entity, new IteratePerfData());
 
+		instantiateSampler.Begin ();
 		var instances = m_EntityManager.Instantiate (entity, 100000);
+		instantiateSampler.End();
 
-		var entity2 = m_EntityManager.AllocateEntity ();
-		m_EntityManager.AddComponent (entity2, new IteratePerfData());
-
-		var instances2 = m_EntityManager.Instantiate (entity2, 100000);
-
-		int sum = 0;
-		getSampler.Begin ();
-		var components = group0.GetComponentDataArray<IteratePerfData>();
-		getSampler.End ();
-
-		iterateSampler.Begin ();
-		for (int i = 0; i < components.Length; ++i)
-			sum += components[i].random;
-		iterateSampler.End ();
-
+		destroySampler.Begin ();
 		m_EntityManager.Destroy (instances);
+		destroySampler.End ();
 
 		instances.Dispose ();
-		instances2.Dispose ();
 
 		UnityEngine.Collections.NativeLeakDetection.Mode = UnityEngine.Collections.NativeLeakDetectionMode.Enabled;
 
--- compute-boid-simulation/Assets/ECS/InstantiatePerfTest/ECSIteratePerformance.cs
+++ compute-boid-simulation/Assets/ECS/InstantiatePerfTest/ECSIteratePerformance.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 using UnityEngine.ECS;
 using UnityEngine.Profiling;
 
-public class InstantiatePerfTest : MonoBehaviour
+public struct IteratePerfData : IComponentData
 {
-	CustomSampler instantiateSampler;
-	CustomSampler destroySampler;
+	public int random;
+}
+public class ECSIteratePerformance : MonoBehaviour
+{
+	CustomSampler iterateSampler;
+	CustomSampler getSampler;
 
 	void Awake()
 	{
-		instantiateSampler = CustomSampler.Create("InstantiateTest");
-		destroySampler = CustomSampler.Create("DestroyTest");
+		iterateSampler = CustomSampler.Create("IterateTest");
+		getSampler = CustomSampler.Create("GetComponentDataTest");
 	}
 
 	void Update()
@@ -21,25 +25,38 @@ public class InstantiatePerfTest : MonoBehaviour
 
 		var oldRoot = DependencyManager.Root;
 		DependencyManager.Root = new DependencyManager ();
-		DependencyManager.SetDefaultCapacity (100000 * 2);
+		DependencyManager.SetDefaultCapacity (100000 * 3);
 
 		var m_EntityManager = DependencyManager.GetBehaviourManager<EntityManager>();
 
-		var group0 = new EntityGroup (m_EntityManager, typeof(BoidSimulations.BoidData));
+		var group0 = new EntityGroup (m_EntityManager, typeof(IteratePerfData));
 		var group1 = new EntityGroup (m_EntityManager, typeof(BoidSimulations.BoidData));
 
 		var entity = m_EntityManager.AllocateEntity ();
 		m_EntityManager.AddComponent (entity, new BoidSimulations.BoidData());
+		m_EntityManager.AddComponent (entity, new IteratePerfData());
 
-		instantiateSampler.Begin ();
 		var instances = m_EntityManager.Instantiate (entity, 100000);
-		instantiateSampler.End();
 
-		destroySampler.Begin ();
+		var entity2 = m_EntityManager.AllocateEntity ();
+		m_EntityManager.AddComponent (entity2, new IteratePerfData());
+
+		var instances2 = m_EntityManager.Instantiate (entity2, 100000);
+
+		int sum = 0;
+		getSampler.Begin ();
+		var components = group0.GetComponentDataArray<IteratePerfData>();
+		getSampler.End ();
+
+		iterateSampler.Begin ();
+		for (int i = 0; i < components.Length; ++i)
+			sum += components[i].random;
+		iterateSampler.End ();
+
 		m_EntityManager.Destroy (instances);
-		destroySampler.End ();
 
 		instances.Dispose ();
+		instances2.Dispose ();
 
 		UnityEngine.Collections.NativeLeakDetection.Mode = UnityEngine.Collections.NativeLeakDetectionMode.Enabled;
 

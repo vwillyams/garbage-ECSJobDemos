9fa14d7790dbe7d639335e3bc7d5bc80d766aa71
Adrian T <ahskbhsk-code@yahoo.com>
2017-09-06 15:37:06 +0300

    fix batched Capacity output

--- compute-boid-simulation/Assets/Packages/NativeContainers/NativeFreeList.cs
+++ compute-boid-simulation/Assets/Packages/NativeContainers/NativeFreeList.cs
@@ -132,27 +132,11 @@ namespace UnityEngine.Collections
 		}
 
 		unsafe public void Add(T value, NativeSlice<int> outputIndices)
-		{			
-			#if ENABLE_NATIVE_ARRAY_CHECKS
-			AtomicSafetyHandle.CheckWriteAndThrow(m_Safety);
-			#endif
-
-			int* outIndicesPtr = (int*)outputIndices.UnsafePtr;
+		{
 			int count = outputIndices.Length;
-
-			if (m_Length + count > m_Capacity)
-				Capacity = (count + m_Length) * 2;
-
-			m_Length += count;
-
-			for (int i = 0; i != count; i++)
+			for (int i = 0; i < count; i++)
 			{
-				int id = m_NextFree;
-				m_NextFree = UnsafeUtility.ReadArrayElementWithStride<int> (m_Buffer, id, m_SizeOf);
-
-				UnsafeUtility.WriteArrayElementWithStride (m_Buffer, id, m_SizeOf, value);
-
-				outIndicesPtr[i] = id;
+				outputIndices[i] = Add(value);
 			}
 		}
 

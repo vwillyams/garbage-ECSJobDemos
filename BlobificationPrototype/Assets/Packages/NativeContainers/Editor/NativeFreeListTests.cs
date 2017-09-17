c4340cb3b33256eee2464820f28fafc0616d7cd4
joeante <joe@Joachims-MacBook-Pro.local>
2017-09-17 15:07:39 +0200

    BlobificationPrototype moved to separate project

---
+++ BlobificationPrototype/Assets/Packages/NativeContainers/Editor/NativeFreeListTests.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Jobs;
+using UnityEngine.Collections;
+#pragma warning disable 0219
+
+public class NativeFreeListTests
+{
+	[Test]
+	public void AddRemove()
+	{
+		var list = new NativeFreeList<int>(Allocator.TempJob);
+		Assert.AreEqual(0, list.Add (0));
+		Assert.AreEqual(1, list.Add (1));
+		Assert.AreEqual(2, list.Add (2));
+		list.Remove (1);
+		Assert.AreEqual (2, list.Length);
+
+		Assert.AreEqual (0, list[0]);
+		Assert.AreEqual (2, list[2]);
+
+		Assert.AreEqual(1, list.Add (3));
+
+		list.Dispose();
+	}
+
+	[Test]
+	public void AutoCapacity()
+	{
+		var list = new NativeFreeList<int>(Allocator.TempJob);
+		for (int i = 0;i!=1000;i++)
+			Assert.AreEqual(i, list.Add (i));
+
+		for (int i = 0;i!=1000;i++)
+			list[i] = i * 2;
+		for (int i = 0;i!=1000;i++)
+			Assert.AreEqual(i * 2, list[i]);
+
+		for (int i = 0;i!=1000;i++)
+			list.Remove (i);
+
+		list.Dispose();
+	}
+
+}
+
+
---
+++ BlobificationPrototype/Assets/Packages/UnityEngine.math/math_unity_conversion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+#pragma warning disable 0660, 0661
+
+namespace UnityEngine
+{
+    public partial struct float3
+    {
+        public static implicit operator Vector3(float3 d) { return new Vector3(d.x, d.y, d.z); }
+        public static implicit operator float3(Vector3 d) { return new float3(d.x, d.y, d.z); }
+    }
+
+    public partial struct float4
+    {
+        public static implicit operator Quaternion(float4 d) { return new Quaternion(d.x, d.y, d.z, d.w); }
+        public static implicit operator float4(Quaternion d) { return new float4(d.x, d.y, d.z, d.w); }
+    }}
+

8cf3a90cde69aa9fc97ba8d0315e1a8928fa8331
joeante <joe@Joachims-MacBook-Pro.local>
2017-09-26 16:47:23 +0200

    Sample IAutoComponentSystemJob …

---
+++ compute-boid-simulation/Assets/ECS/BoidsECS/BoidSystemJob.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ECS;
+using UnityEngine.ECS.Experimental;
+
+namespace BoidSimulations
+{
+    struct BoidSystemJob : IJobProcessComponentData<BoidData>, IAutoComponentSystemJob
+    {
+        float dt;
+        public void Prepare()
+        {
+            dt = Time.deltaTime;
+        }
+
+        public void Execute(ref BoidData boid)
+        {
+//            boid.position.y -= 30 * dt;
+        }
+    }
+}

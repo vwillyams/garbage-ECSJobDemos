dba1c9622c9656f0b5388c71ed35a0d198e35681
joeante <joe@Joachims-MacBook-Pro.local>
2017-10-30 14:12:46 +0100

    Split tests into job safety and normal safety tests

--- ECSJobDemos/Assets/ECS/Editor/ECSSafetyTests.cs
+++ ECSJobDemos/Assets/ECS/Editor/ECSSafetyTests.cs
@@ -14,11 +14,6 @@ namespace UnityEngine.ECS.Tests
 {
     public class ECSSafetyTests : ECSFixture
 	{
-        public ECSSafetyTests()
-        {
-            Assert.IsTrue(Unity.Jobs.LowLevel.Unsafe.JobsUtility.GetJobDebuggerEnabled(), "JobDebugger must be enabled for these tests");
-        }
-
         [Test]
         public void ReadOnlyComponentDataArray()
         {
@@ -44,40 +39,7 @@ namespace UnityEngine.ECS.Tests
             m_Manager.DestroyEntity(entity);
         }
 
-        struct TestIncrementJob : IJob
-        {
-            public ComponentDataArray<EcsTestData> data;
-            public void Execute()
-            {
-                for (int i = 0; i != data.Length;i++)
-                {
-                    var d = data[i];
-                    d.value++;
-                    data[i] = d;
-                }
-            }
-        }
 
-        //@TODO: bad name
-        [Test]
-        public void ComponentDataArrayJobSafety()
-        {
-            var group = m_Manager.CreateComponentGroup(typeof(EcsTestData));
-            var entity = m_Manager.CreateEntity(typeof(EcsTestData));
-            m_Manager.SetComponent(entity, new EcsTestData(42));
-
-            var job = new TestIncrementJob();
-            job.data = group.GetComponentDataArray<EcsTestData>();
-            Assert.AreEqual(42, job.data[0].value);
-
-            var fence = job.Schedule();
-            Assert.Throws<System.InvalidOperationException>(() => { var f = job.data[0].value; });
-
-            fence.Complete();
-            Assert.AreEqual(43, job.data[0].value);
-
-            m_Manager.DestroyEntity(entity);
-        }
 
         [Test]
         public void AccessComponentArrayAfterCreationThrowsException()
@@ -106,105 +68,6 @@ namespace UnityEngine.ECS.Tests
         }
 
         [Test]
-        public void GetComponentCompletesJob()
-        {
-            var entity = m_Manager.CreateEntity(typeof(EcsTestData));
-            var group = m_Manager.CreateComponentGroup(typeof(EcsTestData));
-
-            var job = new TestIncrementJob();
-            job.data = group.GetComponentDataArray<EcsTestData>();
-            group.AddDependency(job.Schedule());
-
-            // Implicit Wait for job, returns value after job has completed.
-            Assert.AreEqual(1, m_Manager.GetComponent<EcsTestData>(entity).value);
-        }
-
-        [Test]
-        public void DestroyEntityCompletesScheduledJobs()
-        {
-            var entity = m_Manager.CreateEntity(typeof(EcsTestData));
-            var entity2 = m_Manager.CreateEntity(typeof(EcsTestData));
-            var group = m_Manager.CreateComponentGroup(typeof(EcsTestData));
-
-            var job = new TestIncrementJob();
-            job.data = group.GetComponentDataArray<EcsTestData>();
-            group.AddDependency(job.Schedule());
-
-            m_Manager.DestroyEntity(entity);
-
-            // @TODO: This is maybe a little bit dodgy way of determining if the job has been completed...
-            //        Probably should expose api to inspector job debugger state...
-            Assert.AreEqual(1, group.GetComponentDataArray<EcsTestData>().Length);
-            Assert.AreEqual(1, group.GetComponentDataArray<EcsTestData>()[0].value);
-        }
-
-        [Test]
-        public void GetComponentDataArrayFromEntitySafety()
-        {
-            var archetype = m_Manager.CreateArchetype(typeof(EcsTestData));
-            var entity = m_Manager.CreateEntity(archetype);
-            m_Manager.SetComponent(entity, new EcsTestData(42));
-
-            var data = m_Manager.GetComponentDataArrayFromEntity<EcsTestData>();
-
-            Assert.IsTrue(data.Exists(entity));
-            Assert.AreEqual(42, data[entity].value);
-
-            data[entity] = new EcsTestData(13);
-
-            m_Manager.DestroyEntity(entity);
-
-            Assert.Throws<InvalidOperationException>(() => { data.Exists(entity); });
-            Assert.Throws<InvalidOperationException>(() => { data[entity] = new EcsTestData(); });
-        }
-
-        [Test]
-        public void EntityManagerDestructionDetectsUnregisteredJob()
-        {
-            LogAssert.Expect(LogType.Error, new System.Text.RegularExpressions.Regex("job is still running"));
-
-            var entity = m_Manager.CreateEntity(typeof(EcsTestData));
-            var group = m_Manager.CreateComponentGroup(typeof(EcsTestData));
-
-            var job = new TestIncrementJob();
-            job.data = group.GetComponentDataArray<EcsTestData>();
-            job.Schedule();
-
-            TearDown();
-        }
-
-        [Test]
-        public void DestroyEntityDetectsUnregisteredJob()
-        {
-            var entity = m_Manager.CreateEntity(typeof(EcsTestData));
-            var group = m_Manager.CreateComponentGroup(typeof(EcsTestData));
-
-            var job = new TestIncrementJob();
-            job.data = group.GetComponentDataArray<EcsTestData>();
-            var fence = job.Schedule();
-
-            Assert.Throws<System.InvalidOperationException>(() => { m_Manager.DestroyEntity(entity); });
-
-            fence.Complete();
-        }
-
-        [Test]
-        public void GetComponentDetectsUnregisteredJob()
-        {
-            var entity = m_Manager.CreateEntity(typeof(EcsTestData));
-            var group = m_Manager.CreateComponentGroup(typeof(EcsTestData));
-
-            var job = new TestIncrementJob();
-            job.data = group.GetComponentDataArray<EcsTestData>();
-            var jobHandle = job.Schedule();
-
-            Assert.Throws<System.InvalidOperationException>(() => { m_Manager.GetComponent<EcsTestData>(entity); });
-
-            jobHandle.Complete();
-        }
-        //@TODO: Similar Add/Remove component checks...
-
-        [Test]
         public void GetSetComponentThrowsIfNotExist()
         {
             var entity = m_Manager.CreateEntity(typeof(EcsTestData));
@@ -217,7 +80,6 @@ namespace UnityEngine.ECS.Tests
             Assert.Throws<System.ArgumentException>(() => { m_Manager.GetComponent<EcsTestData2>(entity); });
             Assert.Throws<System.ArgumentException>(() => { m_Manager.GetComponent<EcsTestData2>(destroyedEntity); });
         }
-
 
         [Test]
         public void ComponentDataArrayFromEntityThrowsIfNotExist()
@@ -233,17 +95,6 @@ namespace UnityEngine.ECS.Tests
 
             Assert.Throws<System.ArgumentException>(() => { var p = data[entity]; });
             Assert.Throws<System.ArgumentException>(() => { var p = data[destroyedEntity]; });
-        }
-
-        //@TODO: AddComponent(Double add), RemoveComponent
-
-        [Test]
-        [Ignore("TODO")]
-        public void ForgetAddJobDependencyIsCaughtInComponentSystem()
-        {
-            throw new System.NotImplementedException();
-            // * Give error immediately about missing AddDependency call?
-            // * Sync against other job even if it was forgotten.
         }
 
     }

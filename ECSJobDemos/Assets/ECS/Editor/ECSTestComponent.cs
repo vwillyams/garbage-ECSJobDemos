f68a42088a148d59efa8704983d7ba6590a1b5be
joeante <joe@Joachims-MacBook-Pro.local>
2017-10-30 23:04:31 +0100

    move test code

--- ECSJobDemos/Assets/ECS/Editor/ECSTestComponent.cs
+++ ECSJobDemos/Assets/ECS/Editor/ECSTestComponent.cs
@@ -14,4 +14,23 @@ namespace UnityEngine.ECS.Tests
 	}
 
 	public class EcsTestComponent : ComponentDataWrapper<EcsTestData> { }
+
+
+    public struct EcsTestData2 : IComponentData
+    {
+        public int value0;
+        public int value1;
+
+        public EcsTestData2(int inValue) { value1 = value0 = inValue; }
+    }
+
+    public struct EcsTestData3 : IComponentData
+    {
+        public int value0;
+        public int value1;
+        public int value2;
+
+        public EcsTestData3(int inValue) { value2 = value1 = value0 = inValue; }
+    }
+
 }
--- ECSJobDemos/Assets/ECS/Editor/ECSTests.cs
+++ ECSJobDemos/Assets/ECS/Editor/ECSTests.cs
@@ -7,29 +7,8 @@ using System;
 
 namespace UnityEngine.ECS.Tests
 {
-	public struct EcsTestData2 : IComponentData
-	{
-		public int value0;
-		public int value1;
-
-		public EcsTestData2(int inValue) { value1 = value0 = inValue; }
-	}
-
-	public struct EcsTestData3 : IComponentData
-	{
-		public int value0;
-		public int value1;
-		public int value2;
-
-		public EcsTestData3(int inValue) { value2 = value1 = value0 = inValue; }
-	}
-
-
-
 	public class ECSCreateAndDestroy : ECSFixture
 	{
-
-
         [Test]
         unsafe public void CreateAndDestroyOne()
         {

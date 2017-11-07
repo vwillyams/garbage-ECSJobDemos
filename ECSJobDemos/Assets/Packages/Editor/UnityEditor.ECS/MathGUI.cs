a06cd6ca07eda314d67926501b363e9af183a4ac
Unknown <messagetodaniel@gmail.com>
2017-11-07 14:02:22 +0100

    Support for all float types

---
+++ ECSJobDemos/Assets/Packages/Editor/UnityEditor.ECS/MathGUI.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using Unity.Mathematics;
+using UnityEngine.ECS;
+using UnityEditor;
+using System;
+using System.Reflection;
+
+public static class MathGUI {
+
+	static readonly string[] vectorFieldNames = new string[]{"x", "y", "z", "w"};
+	static readonly string[] matrixRowNames = new string[] {"m0", "m1", "m2", "m3"};
+
+	public static void FieldGUI(Rect firstCell, object data)
+	{
+		var type = data.GetType();
+		var rows = RowsForType(type);
+		if (rows == 1)
+		{
+			VectorGUI(firstCell, data);
+		}
+		else
+		{
+			MatrixGUI(firstCell, data);
+		}
+	}
+
+	public static void MatrixGUI(Rect firstCell, object data)
+	{
+		var type = data.GetType();
+		var rows = RowsForType(type);
+		var currentCell = firstCell;
+		for (var i = 0; i < rows; ++i)
+		{
+			var field = type.GetField(matrixRowNames[i], BindingFlags.Instance | BindingFlags.Public);
+			var value = field.GetValue(data);
+			VectorGUI(currentCell, value);
+			currentCell.y += firstCell.height;
+		}
+	}
+
+	public static void VectorGUI(Rect firstCell, object data)
+	{
+		var type = data.GetType();
+		var columns = ColumnsForType(type);
+		var currentCell = firstCell;
+		for (var i = 0; i < columns; ++i)
+		{
+			var field = type.GetField(vectorFieldNames[i], BindingFlags.Instance | BindingFlags.Public);
+			var value = (float)field.GetValue(data);
+			ScalarGUI(currentCell, value);
+			currentCell.x += firstCell.width;
+		}
+	}
+
+	public static void ScalarGUI(Rect cell, float data)
+	{
+		EditorGUI.FloatField(cell, GUIContent.none, data);
+	}
+
+	public static int ColumnsForType(Type type)
+	{
+		if (type == typeof(float4) || type == typeof(float4x4))
+			return 4;
+		else if (type == typeof(float3) || type == typeof(float3x3))
+			return 3;
+		else if (type == typeof(float2) || type == typeof(float2x2))
+			return 2;
+		else if (type == typeof(float))
+			return 1;
+		else
+			throw new ArgumentException(string.Format("Type {0} not supported"), type.ToString());
+	}
+
+	public static int RowsForType(Type type)
+	{
+		if (type == typeof(float4x4))
+			return 4;
+		else if (type == typeof(float3x3))
+			return 3;
+		else if (type == typeof(float2x2))
+			return 2;
+		else if (type == typeof(float) || type == typeof(float2) || type == typeof(float3) || type == typeof(float4))
+			return 1;
+		else
+			throw new ArgumentException(string.Format("Type {0} not supported"), type.ToString());
+	}
+}
--- ECSJobDemos/Assets/Packages/Editor/UnityEditor.ECS/StructGUI.cs
+++ ECSJobDemos/Assets/Packages/Editor/UnityEditor.ECS/StructGUI.cs
@@ -14,44 +14,18 @@ public static class StructGUI {
 		var rows = 1;
 		foreach (var field in fields)
 		{
-			displayedFields += ColumnsForField(field.FieldType);
-			rows = Mathf.Max(rows, RowsForType(data.GetType()));
+			displayedFields += MathGUI.ColumnsForType(field.FieldType);
 		}
+		rows = RowsForType(data.GetType());
 		var fieldWidth = rect.width/displayedFields;
 		var fieldHeight = rect.height/rows;
-		var fieldOffset = 0;
+		var currentCell = new Rect(rect.x, rect.y, fieldWidth, fieldHeight);
 		foreach (var field in fields)
 		{
-			if (field.FieldType == typeof(float3))
-			{
-				var f3 = (float3)field.GetValue(data);
-				var v3 = new Vector3(f3.x, f3.y, f3.z);
-				EditorGUI.Vector3Field(new Rect(rect.x + fieldOffset*fieldWidth, rect.y, fieldWidth * 3, fieldHeight), GUIContent.none, v3);
-				fieldOffset += 3;
-			}
-			else if (field.FieldType == typeof(float4x4))
-			{
-				var f4x4 = (float4x4)field.GetValue(data);
-				var v4 = new Vector4(f4x4.m0.x, f4x4.m0.y, f4x4.m0.z, f4x4.m0.w);
-				EditorGUI.Vector4Field(new Rect(rect.x + fieldOffset*fieldWidth, rect.y + fieldHeight * 0, fieldWidth * 3, fieldHeight), GUIContent.none, v4);
-				v4 = new Vector4(f4x4.m1.x, f4x4.m1.y, f4x4.m1.z, f4x4.m1.w);
-				EditorGUI.Vector4Field(new Rect(rect.x + fieldOffset*fieldWidth, rect.y + fieldHeight * 1, fieldWidth * 3, fieldHeight), GUIContent.none, v4);
-				v4 = new Vector4(f4x4.m2.x, f4x4.m2.y, f4x4.m2.z, f4x4.m2.w);
-				EditorGUI.Vector4Field(new Rect(rect.x + fieldOffset*fieldWidth, rect.y + fieldHeight * 2, fieldWidth * 3, fieldHeight), GUIContent.none, v4);
-				v4 = new Vector4(f4x4.m3.x, f4x4.m3.y, f4x4.m3.z, f4x4.m3.w);
-				EditorGUI.Vector4Field(new Rect(rect.x + fieldOffset*fieldWidth, rect.y + fieldHeight * 3, fieldWidth * 3, fieldHeight), GUIContent.none, v4);
-			}
+			var value = field.GetValue(data);
+			MathGUI.FieldGUI(currentCell, value);
+			currentCell.x += fieldWidth * MathGUI.ColumnsForType(field.FieldType);
 		}
-	}
-
-	public static int ColumnsForField(Type type)
-	{
-		if (type == typeof(float4x4))
-			return 4;
-		else if (type == typeof(float3))
-			return 3;
-		else
-			return 1;
 	}
 
 	public static int RowsForType(Type type)
@@ -60,8 +34,7 @@ public static class StructGUI {
 		var rows = 1;
 		foreach (var field in fields)
 		{
-			if (field.FieldType == typeof(float4x4))
-				rows = Mathf.Max(rows, 4);
+			rows = Mathf.Max(rows, MathGUI.RowsForType(field.FieldType));
 		}
 		return rows;
 	}

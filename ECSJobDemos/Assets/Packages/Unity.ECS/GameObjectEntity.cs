f5fbc1d8801034f763197692d7988458dc539253
joeante <joe@Joachims-MacBook-Pro-2.local>
2017-11-19 21:17:32 +0100

    Splitting up ECS a little bit, "Iterator" & "Injection" folders to make things a little more organized

--- ECSJobDemos/Assets/Packages/Unity.ECS/GameObjectEntity.cs
+++ ECSJobDemos/Assets/Packages/Unity.ECS/GameObjectEntity.cs
@@ -8,6 +8,74 @@ using System.Linq;
 
 namespace UnityEngine.ECS
 {
+    
+    //@TODO: This should be fully implemented in C++ for efficiency
+    [RequireComponent(typeof(GameObjectEntity))]
+    public abstract class ComponentDataWrapperBase : MonoBehaviour
+    {
+        abstract internal ComponentType GetComponentType(EntityManager manager);
+        abstract internal void UpdateComponentData(EntityManager manager, Entity entity);
+    }
+
+    //@TODO: This should be fully implemented in C++ for efficiency
+    public class ComponentDataWrapper<T> : ComponentDataWrapperBase where T : struct, IComponentData
+    {
+        [SerializeField]
+        T m_SerializedData;
+
+        public T Value
+        {
+            get
+            { 
+                return m_SerializedData;
+            }
+            set
+            {
+                m_SerializedData = value;
+            }
+        }
+
+
+        internal override ComponentType GetComponentType(EntityManager manager)
+        {
+            return ComponentType.Create<T>();
+        }
+
+        internal override void UpdateComponentData(EntityManager manager, Entity entity)
+        {
+            manager.SetComponent(entity, m_SerializedData);
+        }
+    }
+
+    //@TODO: This should be fully implemented in C++ for efficiency
+    public class SharedComponentDataWrapper<T> : ComponentDataWrapperBase where T : struct, ISharedComponentData
+    {
+        [SerializeField]
+        T m_SerializedData;
+
+        public T Value
+        {
+            get
+            {
+                return m_SerializedData;
+            }
+            set
+            {
+                m_SerializedData = value;
+            }
+        }
+
+
+        internal override ComponentType GetComponentType(EntityManager manager)
+        {
+            return manager.CreateSharedComponentType(m_SerializedData);
+        }
+
+        internal override void UpdateComponentData(EntityManager manager, Entity entity)
+        {
+        }
+    }
+    
     [DisallowMultipleComponent]
     public class GameObjectEntity : MonoBehaviour
     {

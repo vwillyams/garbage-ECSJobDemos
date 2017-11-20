3257471e1a50a3b6a8d68aa03ce70601a96262a2
joeante <joe@Joachims-MacBook-Pro-2.local>
2017-11-20 16:26:53 +0100

    Base fixture for tests

---
+++ UnstablePrototypes/LowLevelQueryAPI/Assets/Unity.Navigation.Tests/NavmeshFixture.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.AI;
+
+namespace Unity.Navigation.Tests
+{
+	public class NavmeshFixture
+	{
+		void Setup()
+		{
+			
+		}
+		
+		void TearDown()
+		{
+			
+		}
+
+	}
+
+	public class NavmeshSafety : NavmeshFixture
+	{
+		[Test]
+		public void NewEditModeTestSimplePasses()
+		{
+//			var query = new 	
+		}
+	}
+}
+

6404f60f2201f8851470665a43c8e70a97333f8f
Adrian T <ahskbhsk-code@yahoo.com>
2017-11-20 16:42:39 +0100

    [ai] Added setup for NavMesh test fixture.

--- UnstablePrototypes/LowLevelQueryAPI/Assets/Unity.Navigation.Tests/NavmeshFixture.cs
+++ UnstablePrototypes/LowLevelQueryAPI/Assets/Unity.Navigation.Tests/NavmeshFixture.cs
@@ -3,23 +3,41 @@ using UnityEditor;
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.AI;
 
 namespace Unity.Navigation.Tests
 {
-	public class NavmeshFixture
-	{
-		void Setup()
-		{
-			
-		}
-		
-		void TearDown()
-		{
-			
-		}
+    public class NavmeshFixture
+    {
+        NavMeshData m_NavMesh;
+        NavMeshDataInstance m_NavMeshInstances;
+        const int k_AreaWalking = 0;
+        const float k_Height = 0.3f;
 
-	}
+        void Setup()
+        {
+            var boxSource1 = new NavMeshBuildSource
+            {
+                shape = NavMeshBuildSourceShape.Box,
+                size = new Vector3(10, k_Height, 10),
+                area = k_AreaWalking,
+                transform = Matrix4x4.identity
+            };
+            var sources = new List<NavMeshBuildSource> { boxSource1 };
+            var bounds = new Bounds(Vector3.zero, 100.0f * Vector3.one);
+            var settings = NavMesh.GetSettingsByID(0);
+            m_NavMesh = NavMeshBuilder.BuildNavMeshData(settings, sources, bounds, Vector3.zero, Quaternion.identity);
+
+            m_NavMeshInstances = NavMesh.AddNavMeshData(m_NavMesh);
+        }
+
+        void TearDown()
+        {
+            m_NavMeshInstances.Remove();
+        }
+
+    }
 
 	public class NavmeshSafety : NavmeshFixture
 	{
@@ -30,4 +48,3 @@ namespace Unity.Navigation.Tests
 		}
 	}
 }
-

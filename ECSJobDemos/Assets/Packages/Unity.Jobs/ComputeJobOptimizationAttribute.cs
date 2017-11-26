cd992e72a4c3a2472ecbdfff668a036b1bbfe4b3
joeante <joe@Joachims-MacBook-Pro-2.local>
2017-11-26 02:23:12 +0100

    Prepare for making the demo ready to drop in the burst compiler package

---
+++ ECSJobDemos/Assets/Packages/Unity.Jobs/ComputeJobOptimizationAttribute.cs
@@ -0,0 +1,34 @@
+﻿namespace Unity.Jobs
+{
+    public enum Support
+    {
+        Strict,
+        Relaxed
+    }
+
+    public enum Accuracy
+    {
+        Low,
+        Med,
+        High,
+        Std
+    }
+
+    public class ComputeJobOptimizationAttribute : System.Attribute
+    {
+        internal Support m_Support;
+        internal Accuracy m_Accuracy;
+
+        public ComputeJobOptimizationAttribute()
+        {
+            m_Support = Support.Strict;
+            m_Accuracy = Accuracy.Std;
+        }
+
+        public ComputeJobOptimizationAttribute(Accuracy accuracy, Support support)
+        {
+            m_Support = support;
+            m_Accuracy = accuracy;
+        }
+    }
+}

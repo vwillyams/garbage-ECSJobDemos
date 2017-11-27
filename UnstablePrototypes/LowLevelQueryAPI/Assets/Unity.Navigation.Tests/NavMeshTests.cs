fd364e252619ddfc761230e6fed498901faa0e8c
Adrian T <ahskbhsk-code@yahoo.com>
2017-11-27 18:06:15 +0100

    [ai] Rename NavMesh_Exists test to NavMesh_WhenCreated_IsAvailable .

--- UnstablePrototypes/LowLevelQueryAPI/Assets/Unity.Navigation.Tests/NavMeshTests.cs
+++ UnstablePrototypes/LowLevelQueryAPI/Assets/Unity.Navigation.Tests/NavMeshTests.cs
@@ -1,15 +1,9 @@
 using UnityEngine;
 using NUnit.Framework;
-using System.Collections.Generic;
 using UnityEngine.AI;
-using UnityEngine.Experimental.AI;
-using Unity.Collections;
-using Object = UnityEngine.Object;
 
 namespace Unity.Navigation.Tests
 {
-   
-
 	public class NavMeshTests : NavMeshFixture
 	{
 		[Test]
@@ -20,13 +14,12 @@ namespace Unity.Navigation.Tests
 			NavMesh.RemoveNavMeshData(m_NavMeshInstance);
 			NavMesh.RemoveNavMeshData(m_NavMeshInstance);
 		}
-	    
-	
+
+
 		[Test]
-		public void NavMesh_Exists()
+		public void NavMesh_WhenCreated_IsAvailable()
 		{
-			//@TODO: We have two different NavMeshHit... THATS BAD
-			UnityEngine.AI.NavMeshHit hit;
+			NavMeshHit hit;
 			var center = new Vector3(0, k_Height, 0);
 			var found = NavMesh.SamplePosition(center, out hit, 1, NavMesh.AllAreas);
 			Assert.IsTrue(found, string.Format("NavMesh was not found at position {0}.", center));

031cc46dfd4cd626444a021a919b4ff3e16d0abe
joeante <joe@unity3d.com>
2017-12-16 18:50:24 +0100

    Updated instantiate performance tests

--- ECSJobDemos/Assets/ECS/InstantiatePerformanceTest/ECSInstantiatePerformance.cs
+++ ECSJobDemos/Assets/ECS/InstantiatePerformanceTest/ECSInstantiatePerformance.cs
@@ -1,260 +1,73 @@
 ﻿using UnityEngine;
 using Unity.Collections;
-using Unity.Jobs;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine.ECS;
 using UnityEngine.Profiling;
-using Unity.Collections.LowLevel.Unsafe;
-using Unity.Mathematics;
-using System;
 using Unity.Jobs.LowLevel.Unsafe;
-
-// 64 + 16 + 12 + 128 + 4 + 4 = 228 bytes
-
-struct Component64Bytes : IComponentData
-{
-    float4x4 value;
-}
-
-struct Component16Bytes : IComponentData
-{
-    float4 value;
-}
-
-struct Component12Bytes : IComponentData
-{
-    public float3 value;
-}
-
-struct Component4Bytes : IComponentData
-{
-	public float value;
-}
-
-struct Component4BytesDst : IComponentData
-{
-	public float value;
-
-	public Component4BytesDst(float v)
-	{
-		value = v;
-	}
-}
-
-struct Component128Bytes : IComponentData
-{
-    float4x4 value0;
-    float4x4 value1;
-}
 
 public class ECSInstantiatePerformance : MonoBehaviour
 {
-	unsafe struct EntityIter
-	{
-		public Component4Bytes* 	src;
-		public Component4BytesDst* 	dst;
-	}
-
-	[ComputeJobOptimization]
-	struct Iterate_ComponentDataArray : IJob
-	{
-		public ComponentDataArray<Component4Bytes> 		src;
-		public ComponentDataArray<Component4BytesDst> 	dst;
-
-		public void Execute()
-		{
-			for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
-			{
-				//@TODO: Ref returns upgrade
-				for (int i = 0; i < src.Length; ++i)
-					dst[i] = new Component4BytesDst(src[i].value);
-			}
-		}
-	}
-
-	[ComputeJobOptimization]
-	struct Iterate_ProcessEntities : IJobProcessEntities<EntityIter>
-	{
-		unsafe public void Execute(EntityIter entity)
-		{
-			entity.dst->value = entity.src->value;
-		}
-	}
-
-	//@TODO: Burst can't do this yet
-	//[ComputeJobOptimization]
-	struct Iterate_ForEachEntities : IJob
-	{
-		public ComponentGroupArray<EntityIter> entities;
-
-		unsafe public void Execute()
-		{
-			foreach (var entity in entities)
-				entity.dst->value = entity.src->value;
-		}
-	}
-
-	[ComputeJobOptimization]
-	struct Iterate_ProcessComponentData : IJobProcessComponentData<Component4Bytes, Component4BytesDst>
-	{
-		public void Execute(ref Component4Bytes src, ref Component4BytesDst dst)
-		{
-			dst.value = src.value;
-		}
-	}
-
-	[ComputeJobOptimization]
-	unsafe struct Iterate_FloatPointer : IJob
-	{
-		[NativeDisableUnsafePtrRestriction]
-		public float* 	src;
-		[NativeDisableUnsafePtrRestriction]
-		public float* 	dst;
-		public int 		length;
-
-		public void Execute()
-		{
-			for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
-			{
-				for (int i = 0; i < length; ++i)
-					dst[i] = src[i];
-			}
-		}
-	}
-
-	[ComputeJobOptimization]
-	struct Iterate_NativeArrayParallelFor : IJobParallelFor
-	{
-		[NativeMatchesParallelForLength]
-		public NativeArray<float> 	src;
-
-		[NativeMatchesParallelForLength]
-		public NativeArray<float> 	dst;
-
-		public void Execute(int i)
-		{
-			dst[i] = src[i];
-		}
-	}
-
-	[ComputeJobOptimization]
-	struct Iterate_NativeArray : IJob
-	{
-		public NativeArray<float> 	src;
-		public NativeArray<float> 	dst;
-
-		public void Execute()
-		{
-			for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
-			{
-				for (int i = 0; i < dst.Length; ++i)
-					dst[i] = src[i];
-			}
-		}
-	}
-
-	[ComputeJobOptimization]
-	struct Iterate_NativeSlice : IJob
-	{
-		public NativeSlice<float> 	src;
-		public NativeSlice<float> 	dst;
-
-		public void Execute()
-		{
-			for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
-			{
-				for (int i = 0; i < dst.Length; ++i)
-					dst[i] = src[i];
-			}
-		}
-	}
-
 	CustomSampler setupSampler;
 	CustomSampler instantiateSampler;
 	CustomSampler destroySampler;
     CustomSampler memcpySampler;
-	CustomSampler instantiateMemcpySampler ;
-	CustomSampler iterateArraySampler;
+    CustomSampler instantiateMemcpySampler ;
+    CustomSampler instantiateMemcpyReplicateSampler ;
 
     void Awake()
 	{
 		setupSampler = CustomSampler.Create("Setup");
 		instantiateSampler = CustomSampler.Create("InstantiateTest");
-		instantiateMemcpySampler = CustomSampler.Create("InstantiateTest - Memcpy");
+	    instantiateMemcpySampler = CustomSampler.Create("InstantiateTest - Memcpy");
+	    instantiateMemcpyReplicateSampler = CustomSampler.Create("InstantiateTest - MemcpyReplicate");
 		destroySampler = CustomSampler.Create("DestroyTest");
         memcpySampler = CustomSampler.Create("Iterate - Memcpy");
-		iterateArraySampler = CustomSampler.Create("Iterate - float[]");
     }
 
-	unsafe void TestManagedArray()
-	{
-		setupSampler.Begin();
-		var src = new float[PerformanceTestConfiguration.InstanceCount];
-		var dst = new float[PerformanceTestConfiguration.InstanceCount];
-		setupSampler.End();
+    unsafe void TestUnsafePtr()
+    {
+        setupSampler.Begin();
+        int[] sizes =
+        {
+            sizeof(Component128Bytes), sizeof(Component12Bytes), sizeof(Component64Bytes), sizeof(Component16Bytes),
+            sizeof(Component4Bytes), sizeof(Component4BytesDst), sizeof(Entity),
+            20 // overhead of EntityData lookup table
+        };
 
-		iterateArraySampler.Begin();
-		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
-		{
-			for (int i = 0; i != src.Length; i++)
-				dst[i] = src[i];
-		}
-		iterateArraySampler.End();
-	}
+        int size = 0;
+        foreach (var s in sizes)
+            size += s;
+        size *= PerformanceTestConfiguration.InstanceCount;
+        var src = (float*)UnsafeUtility.Malloc(size, 64, Allocator.Persistent);
+        UnsafeUtility.MemClear(src, size);
 
-	unsafe void TestNativeArray()
-	{
-		setupSampler.Begin();
-		var src = new NativeArray<float>(PerformanceTestConfiguration.InstanceCount, Allocator.Persistent);
-		var dst = new NativeArray<float>(PerformanceTestConfiguration.InstanceCount, Allocator.Persistent);
-		setupSampler.End();
+        var dst = (float*)UnsafeUtility.Malloc(size, 64, Allocator.Persistent);
+        setupSampler.End();
 
-		var jobArray = new Iterate_NativeArray() { src = src, dst = dst };
-		jobArray.Run();
+        instantiateMemcpySampler.Begin();
+        for (int i = 0; i < PerformanceTestConfiguration.Iterations; i++)
+            UnsafeUtility.MemCpy(dst, src, size);
+        instantiateMemcpySampler.End();
 
-		var jobArrayParallellFor = new Iterate_NativeArrayParallelFor() { src = src, dst = dst };
-		for (int i = 0;i != PerformanceTestConfiguration.Iterations;i++)
-			jobArrayParallellFor.Run(src.Length);
+        instantiateMemcpyReplicateSampler.Begin();
+        for (int i = 0; i < PerformanceTestConfiguration.Iterations; i++)
+        {
+            int position = 0;
+            foreach (var elementSize in sizes)
+            {
+                UnsafeUtility.MemCpyReplicate((byte*)dst + position, src, elementSize, PerformanceTestConfiguration.InstanceCount);
+                position += elementSize * PerformanceTestConfiguration.InstanceCount;
+            }
 
-		var jobSlice = new Iterate_NativeSlice() { src = src, dst = dst };
-		jobSlice.Run();
+        }
+        instantiateMemcpyReplicateSampler.End();
 
-		setupSampler.Begin();
-		src.Dispose();
-		dst.Dispose();
-		setupSampler.End();
-	}
 
-	unsafe void TestUnsafePtr()
-	{
-		setupSampler.Begin();
-		int size = sizeof(Component4Bytes) + sizeof(Component16Bytes)  + sizeof(Component64Bytes) + sizeof(Component12Bytes) + sizeof(Component128Bytes) + sizeof(Entity);
-		size *= PerformanceTestConfiguration.InstanceCount;
-		var src = (float*)UnsafeUtility.Malloc(size, 64, Allocator.Persistent);
-		UnsafeUtility.MemClear(src, size);
-
-		var dst = (float*)UnsafeUtility.Malloc(size, 64, Allocator.Persistent);
-		setupSampler.End();
-
-		instantiateMemcpySampler.Begin();
-		UnsafeUtility.MemCpy(dst, src, size);
-		instantiateMemcpySampler.End();
-
-		memcpySampler.Begin();
-		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
-		{
-			UnsafeUtility.MemCpy(dst, src, sizeof(float) * PerformanceTestConfiguration.InstanceCount);
-		}
-		memcpySampler.End();
-
-		var jobFloatPtr = new Iterate_FloatPointer() { src = src, dst = dst, length = PerformanceTestConfiguration.InstanceCount };
-		jobFloatPtr.Run();
-
-		setupSampler.Begin();
-		UnsafeUtility.Free(src, Allocator.Persistent);
-		UnsafeUtility.Free(dst, Allocator.Persistent);
-		setupSampler.End();
-	}
-
+        setupSampler.Begin();
+        UnsafeUtility.Free(src, Allocator.Persistent);
+        UnsafeUtility.Free(dst, Allocator.Persistent);
+        setupSampler.End();
+    }
 	unsafe void TestEntities()
 	{
 		setupSampler.Begin();
@@ -268,45 +81,24 @@ public class ECSInstantiatePerformance : MonoBehaviour
 
 		var entityManager = World.Active.GetOrCreateManager<EntityManager>();
 
-		setupSampler.End();
-
-		setupSampler.Begin();
-		var archetype = entityManager.CreateEntity(typeof(Component128Bytes), typeof(Component12Bytes), typeof(Component64Bytes), typeof(Component16Bytes), typeof(Component4Bytes), typeof(Component4BytesDst));
+	    var archetype = entityManager.CreateEntity(typeof(Component128Bytes), typeof(Component12Bytes), typeof(Component64Bytes), typeof(Component16Bytes), typeof(Component4Bytes), typeof(Component4BytesDst));
 		var group = entityManager.CreateComponentGroup(typeof(Component4Bytes), typeof(Component4BytesDst));
+
+	    var instances = new NativeArray<Entity>(PerformanceTestConfiguration.InstanceCount - 1, Allocator.Temp);
 		setupSampler.End();
 
-		instantiateSampler.Begin ();
-		var instances = new NativeArray<Entity>(PerformanceTestConfiguration.InstanceCount - 1, Allocator.Temp);
-		entityManager.Instantiate (archetype, instances);
-		instantiateSampler.End();
 
-		var src = group.GetComponentDataArray<Component4Bytes>();
-		var dst = group.GetComponentDataArray<Component4BytesDst>();
 
-		var componentDataArrayJob = new Iterate_ComponentDataArray() { src = src, dst = dst };
-		componentDataArrayJob.Run();
+	    for (int i = 0; i < PerformanceTestConfiguration.Iterations; i++)
+	    {
+	        instantiateSampler.Begin ();
+	        entityManager.Instantiate (archetype, instances);
+	        instantiateSampler.End();
 
-		var componentProcessDataJob = new Iterate_ProcessComponentData();
-		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
-			componentProcessDataJob.Run(src, dst);
-
-		var entityArrayCache = new ComponentGroupArrayStaticCache(typeof(EntityIter), entityManager);
-		var entityArray = new ComponentGroupArray<EntityIter>(entityArrayCache);
-
-		var componentProcessEntities = new Iterate_ProcessEntities();
-		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
-			componentProcessEntities.Run(entityArray);
-
-		var componentForEachEntities = new Iterate_ForEachEntities();
-		componentForEachEntities.entities = entityArray;
-		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
-			componentForEachEntities.Run();
-
-		entityArrayCache.Dispose();
-
-		destroySampler.Begin ();
-		entityManager.DestroyEntity (instances);
-		destroySampler.End ();
+	        destroySampler.Begin ();
+	        entityManager.DestroyEntity (instances);
+	        destroySampler.End ();
+	    }
 
 		setupSampler.Begin();
 		entityManager.DestroyEntity (archetype);
@@ -328,10 +120,8 @@ public class ECSInstantiatePerformance : MonoBehaviour
 	    var wasEnabled = JobsUtility.JobDebuggerEnabled;
 	    JobsUtility.JobDebuggerEnabled = false;
 
-	    TestUnsafePtr();
-	    TestManagedArray();
-	    TestNativeArray();
 	    TestEntities();
+        TestUnsafePtr();
 
 	    JobsUtility.JobDebuggerEnabled = wasEnabled;
     }
---
+++ ECSJobDemos/Assets/ECS/InstantiatePerformanceTest/ECSIteratePerformance.cs
@@ -0,0 +1,325 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine.ECS;
+using UnityEngine.Profiling;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
+using Unity.Jobs.LowLevel.Unsafe;
+
+// 64 + 16 + 12 + 128 + 4 + 4 = 228 bytes
+
+struct Component64Bytes : IComponentData
+{
+    float4x4 value;
+}
+
+struct Component16Bytes : IComponentData
+{
+    float4 value;
+}
+
+struct Component12Bytes : IComponentData
+{
+    public float3 value;
+}
+
+struct Component4Bytes : IComponentData
+{
+	public float value;
+}
+
+struct Component4BytesDst : IComponentData
+{
+	public float value;
+
+	public Component4BytesDst(float v)
+	{
+		value = v;
+	}
+}
+
+struct Component128Bytes : IComponentData
+{
+    float4x4 value0;
+    float4x4 value1;
+}
+
+public class ECSIteratePerformance : MonoBehaviour
+{
+	unsafe struct EntityIter
+	{
+		public Component4Bytes* 	src;
+		public Component4BytesDst* 	dst;
+	}
+
+	[ComputeJobOptimization]
+	struct Iterate_ComponentDataArray : IJob
+	{
+		public ComponentDataArray<Component4Bytes> 		src;
+		public ComponentDataArray<Component4BytesDst> 	dst;
+
+		public void Execute()
+		{
+			for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
+			{
+				//@TODO: Ref returns upgrade
+				for (int i = 0; i < src.Length; ++i)
+					dst[i] = new Component4BytesDst(src[i].value);
+			}
+		}
+	}
+
+	[ComputeJobOptimization]
+	struct Iterate_ProcessEntities : IJobProcessEntities<EntityIter>
+	{
+		unsafe public void Execute(EntityIter entity)
+		{
+			entity.dst->value = entity.src->value;
+		}
+	}
+
+	//@TODO: Burst can't do this yet
+	//[ComputeJobOptimization]
+	struct Iterate_ForEachEntities : IJob
+	{
+		public ComponentGroupArray<EntityIter> entities;
+
+		unsafe public void Execute()
+		{
+			foreach (var entity in entities)
+				entity.dst->value = entity.src->value;
+		}
+	}
+
+	[ComputeJobOptimization]
+	struct Iterate_ProcessComponentData : IJobProcessComponentData<Component4Bytes, Component4BytesDst>
+	{
+		public void Execute(ref Component4Bytes src, ref Component4BytesDst dst)
+		{
+			dst.value = src.value;
+		}
+	}
+
+	[ComputeJobOptimization]
+	unsafe struct Iterate_FloatPointer : IJob
+	{
+		[NativeDisableUnsafePtrRestriction]
+		public float* 	src;
+		[NativeDisableUnsafePtrRestriction]
+		public float* 	dst;
+		public int 		length;
+
+		public void Execute()
+		{
+			for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
+			{
+				for (int i = 0; i < length; ++i)
+					dst[i] = src[i];
+			}
+		}
+	}
+
+	[ComputeJobOptimization]
+	struct Iterate_NativeArrayParallelFor : IJobParallelFor
+	{
+		[NativeMatchesParallelForLength]
+		public NativeArray<float> 	src;
+
+		[NativeMatchesParallelForLength]
+		public NativeArray<float> 	dst;
+
+		public void Execute(int i)
+		{
+			dst[i] = src[i];
+		}
+	}
+
+	[ComputeJobOptimization]
+	struct Iterate_NativeArray : IJob
+	{
+		public NativeArray<float> 	src;
+		public NativeArray<float> 	dst;
+
+		public void Execute()
+		{
+			for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
+			{
+				for (int i = 0; i < dst.Length; ++i)
+					dst[i] = src[i];
+			}
+		}
+	}
+
+	[ComputeJobOptimization]
+	struct Iterate_NativeSlice : IJob
+	{
+		public NativeSlice<float> 	src;
+		public NativeSlice<float> 	dst;
+
+		public void Execute()
+		{
+			for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
+			{
+				for (int i = 0; i < dst.Length; ++i)
+					dst[i] = src[i];
+			}
+		}
+	}
+
+	CustomSampler setupSampler;
+    CustomSampler memcpySampler;
+    CustomSampler instantiateMemcpySampler ;
+    CustomSampler instantiateMemcpyReplicateSampler ;
+	CustomSampler iterateArraySampler;
+
+    void Awake()
+	{
+		setupSampler = CustomSampler.Create("Setup");
+	    instantiateMemcpySampler = CustomSampler.Create("InstantiateTest - Memcpy");
+	    instantiateMemcpyReplicateSampler = CustomSampler.Create("InstantiateTest - MemcpyReplicate");
+        memcpySampler = CustomSampler.Create("Iterate - Memcpy");
+		iterateArraySampler = CustomSampler.Create("Iterate - float[]");
+    }
+
+	unsafe void TestManagedArray()
+	{
+		setupSampler.Begin();
+		var src = new float[PerformanceTestConfiguration.InstanceCount];
+		var dst = new float[PerformanceTestConfiguration.InstanceCount];
+		setupSampler.End();
+
+		iterateArraySampler.Begin();
+		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
+		{
+			for (int i = 0; i != src.Length; i++)
+				dst[i] = src[i];
+		}
+		iterateArraySampler.End();
+	}
+
+	unsafe void TestNativeArray()
+	{
+		setupSampler.Begin();
+		var src = new NativeArray<float>(PerformanceTestConfiguration.InstanceCount, Allocator.Persistent);
+		var dst = new NativeArray<float>(PerformanceTestConfiguration.InstanceCount, Allocator.Persistent);
+		setupSampler.End();
+
+		var jobArray = new Iterate_NativeArray() { src = src, dst = dst };
+		jobArray.Run();
+
+		var jobArrayParallellFor = new Iterate_NativeArrayParallelFor() { src = src, dst = dst };
+		for (int i = 0;i != PerformanceTestConfiguration.Iterations;i++)
+			jobArrayParallellFor.Run(src.Length);
+
+		var jobSlice = new Iterate_NativeSlice() { src = src, dst = dst };
+		jobSlice.Run();
+
+		setupSampler.Begin();
+		src.Dispose();
+		dst.Dispose();
+		setupSampler.End();
+	}
+
+	unsafe void TestUnsafePtr()
+	{
+		setupSampler.Begin();
+
+		int size = sizeof(float) * PerformanceTestConfiguration.InstanceCount;
+		var src = (float*)UnsafeUtility.Malloc(size, 64, Allocator.Persistent);
+		UnsafeUtility.MemClear(src, size);
+
+		var dst = (float*)UnsafeUtility.Malloc(size, 64, Allocator.Persistent);
+		setupSampler.End();
+
+		memcpySampler.Begin();
+		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
+		{
+			UnsafeUtility.MemCpy(dst, src, sizeof(float) * PerformanceTestConfiguration.InstanceCount);
+		}
+		memcpySampler.End();
+
+		var jobFloatPtr = new Iterate_FloatPointer() { src = src, dst = dst, length = PerformanceTestConfiguration.InstanceCount };
+		jobFloatPtr.Run();
+
+		setupSampler.Begin();
+		UnsafeUtility.Free(src, Allocator.Persistent);
+		UnsafeUtility.Free(dst, Allocator.Persistent);
+		setupSampler.End();
+	}
+
+	unsafe void TestEntities()
+	{
+		setupSampler.Begin();
+		World oldRoot = null;
+		if (PerformanceTestConfiguration.CleanManagers)
+		{
+			oldRoot = World.Active;
+			World.Active = new World();
+			World.Active.SetDefaultCapacity(PerformanceTestConfiguration.InstanceCount * 2);
+		}
+
+		var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+		var archetype = entityManager.CreateEntity(typeof(Component128Bytes), typeof(Component12Bytes), typeof(Component64Bytes), typeof(Component16Bytes), typeof(Component4Bytes), typeof(Component4BytesDst));
+		var group = entityManager.CreateComponentGroup(typeof(Component4Bytes), typeof(Component4BytesDst));
+
+	    var instances = new NativeArray<Entity>(PerformanceTestConfiguration.InstanceCount - 1, Allocator.Temp);
+
+	    entityManager.Instantiate (archetype, instances);
+	    setupSampler.End();
+
+		var src = group.GetComponentDataArray<Component4Bytes>();
+		var dst = group.GetComponentDataArray<Component4BytesDst>();
+
+		var componentDataArrayJob = new Iterate_ComponentDataArray() { src = src, dst = dst };
+		componentDataArrayJob.Run();
+
+		var componentProcessDataJob = new Iterate_ProcessComponentData();
+		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
+			componentProcessDataJob.Run(src, dst);
+
+		var entityArrayCache = new ComponentGroupArrayStaticCache(typeof(EntityIter), entityManager);
+		var entityArray = new ComponentGroupArray<EntityIter>(entityArrayCache);
+
+		var componentProcessEntities = new Iterate_ProcessEntities();
+		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
+			componentProcessEntities.Run(entityArray);
+
+		var componentForEachEntities = new Iterate_ForEachEntities();
+		componentForEachEntities.entities = entityArray;
+		for (int iter = 0; iter != PerformanceTestConfiguration.Iterations; iter++)
+			componentForEachEntities.Run();
+
+		entityArrayCache.Dispose();
+
+	    setupSampler.Begin ();
+		entityManager.DestroyEntity (instances);
+
+		entityManager.DestroyEntity (archetype);
+
+		instances.Dispose ();
+		group.Dispose();
+
+		if (oldRoot != null)
+		{
+			World.Active.Dispose();
+			World.Active = oldRoot;
+		}
+		setupSampler.End();
+	}
+
+
+	unsafe void Update()
+    {
+	    var wasEnabled = JobsUtility.JobDebuggerEnabled;
+	    JobsUtility.JobDebuggerEnabled = false;
+
+	    TestUnsafePtr();
+	    TestManagedArray();
+	    TestNativeArray();
+	    TestEntities();
+
+	    JobsUtility.JobDebuggerEnabled = wasEnabled;
+    }
+}

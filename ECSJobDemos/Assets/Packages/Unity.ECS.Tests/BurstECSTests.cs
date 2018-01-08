6cacb718d1d25aac7a712e117ef37e437b5ad10e
Andreas Fredriksson <andreasf@unity3d.com>
2018-01-08 09:39:43 -0800

    Add starting point for some component group data disasm test

---
+++ ECSJobDemos/Assets/Packages/Unity.ECS.Tests/BurstECSTests.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+
+namespace UnityEngine.ECS.Tests
+{
+    public class BurstECSTests
+    {
+
+    public struct SimpleComponentData : IComponentData
+    {
+        public int val;
+    }
+
+    [ComputeJobOptimization(Accuracy = Accuracy.Low, Support = Support.Relaxed)]
+    public struct SimpleComponentDataTest : IJob
+    {
+        [ReadOnly]
+        public ComponentDataArray<SimpleComponentData> a;
+
+        public ComponentDataArray<SimpleComponentData> b;
+
+        public void Execute()
+        {
+            for (int i = 0; i < a.Length; ++i)
+            {
+                var c = b[i];
+                c.val = a[i].val + 1;
+                b[i] = c;
+            }
+        }
+    }
+
+    }
+}

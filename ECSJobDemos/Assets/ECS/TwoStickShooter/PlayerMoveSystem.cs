3ab5f79f7906826a27c11747365719f10f2ea4ca
Andreas Fredriksson <andreasf@unity3d.com>
2018-01-26 16:48:36 -0800

    Moving the player.

---
+++ ECSJobDemos/Assets/ECS/TwoStickShooter/PlayerMoveSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.Jobs;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.ECS;
+using UnityEngine.XR.WSA;
+
+namespace TwoStickExample
+{
+
+    public class PlayerMoveSystem : JobComponentSystem
+    {
+        public struct Data
+        {
+            public int Length;
+            public ComponentDataArray<WorldPos> Position;
+            [ReadOnly]
+            public ComponentDataArray<PlayerInput> Input;
+        }
+
+        [InjectComponentGroup] private Data m_Data;
+
+        public struct Job : IJobParallelFor
+        {
+            public ComponentDataArray<WorldPos> Position;
+            [ReadOnly]
+            public ComponentDataArray<PlayerInput> Input;
+            public float DeltaTime;
+
+            public void Execute(int index)
+            {
+                WorldPos pos = Position[index];
+
+                pos.Position += DeltaTime * Input[index].Move;
+                pos.Heading = Input[index].Shoot;
+
+                Position[index] = pos;
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var job = new Job
+            {
+                Position = m_Data.Position,
+                Input = m_Data.Input,
+                DeltaTime = Time.deltaTime
+            };
+
+            return job.Schedule(m_Data.Length, 64, inputDeps);
+        }
+    }
+}

b883e572f3e6b65651b36c354060d7e32d54e99c
Henrik Poulsen <henrikp@unity3d.com>
2018-01-29 15:30:45 +0100

    Fixed standalone compilation error caused by a "using Microsoft.Msagl" that snuck in from somewhere

--- ECSJobDemos/Assets/ECS/SimpleMovement/PositionConstraintSystem.cs
+++ ECSJobDemos/Assets/ECS/SimpleMovement/PositionConstraintSystem.cs
@@ -1,9 +1,6 @@
-﻿using Boo.Lang;
-using Microsoft.Msagl.Core.Layout.ProximityOverlapRemoval.StressEnergy;
-using Unity.Collections;
+﻿using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
-using UnityEngine.ECS;
 using UnityEngine.ECS.Transform;
 
 namespace UnityEngine.ECS.SimpleMovement
@@ -20,7 +17,7 @@ namespace UnityEngine.ECS.SimpleMovement
         }
 
         [Inject] private PositionConstraintsGroup m_PositionContraintsGroup;
-        
+
         struct PositionsGroup
         {
             public ComponentDataArray<TransformPosition> positions;
@@ -30,7 +27,7 @@ namespace UnityEngine.ECS.SimpleMovement
         }
 
         [Inject] private PositionsGroup m_PositionsGroup;
-        
+
         [ComputeJobOptimization]
         struct ContrainPositions : IJob
         {
@@ -50,13 +47,13 @@ namespace UnityEngine.ECS.SimpleMovement
                 {
                     int childIndex = 0;
                     int parentIndex = 0;
-                    
+
                     entityIndexHashMap.TryGetValue(positionConstraintEntities[i], out childIndex);
                     entityIndexHashMap.TryGetValue(positionConstraints[i].parentEntity, out parentIndex);
-                    
+
                     var childPosition = positions[childIndex].position;
                     var parentPosition = positions[parentIndex].position;
-                    
+
                     float3 d = childPosition - parentPosition;
                     float len = math.length(d);
                     float nl = math.min(math.max(len, positionConstraints[i].minDistance),
@@ -90,11 +87,11 @@ namespace UnityEngine.ECS.SimpleMovement
             constrainPositionsJob.positionConstraintEntities = m_PositionContraintsGroup.entities;
             constrainPositionsJob.entityIndexHashMap = m_EntityIndexHashMap.Value;
             return constrainPositionsJob.Schedule(inputDeps);
-        } 
-        
+        }
+
         NativeHashMap<Entity, int>? m_EntityIndexHashMap;
         int m_PositionOrderVersion;
-        
+
         protected override void OnCreateManager(int capacity)
         {
             base.OnCreateManager(capacity);

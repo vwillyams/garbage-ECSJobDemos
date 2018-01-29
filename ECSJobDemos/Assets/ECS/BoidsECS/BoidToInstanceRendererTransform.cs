290de331ea75d958d7f0e25705bfcf0f3d58ffb6
joeante <joe@unity3d.com>
2018-01-29 22:38:57 +0100

    BoidToInstanceRendererTransform with all the different possible combinations

--- ECSJobDemos/Assets/ECS/BoidsECS/BoidToInstanceRendererTransform.cs
+++ ECSJobDemos/Assets/ECS/BoidsECS/BoidToInstanceRendererTransform.cs
@@ -9,23 +9,132 @@ using UnityEngine.ECS.Transform;
 
 namespace BoidSimulations
 {
-#if true
-    
     // Transforms BoidData simulation state to TransformMatrix
-    // which is essentially a matrix used by the rendering system.
-    [UpdateBefore(typeof(BoidSimulationSystem))]
-    class BoidToInstanceRendererTransform : JobComponentSystem
+    // TransformMatrix is used by the rendering system.
+
+    
+    [DisableAutoCreation]
+    class BoidToInstanceRendererTransform_GetEntities : ComponentSystem
+    {
+        unsafe struct Group
+        {
+            [ReadOnly] 
+            public BoidData*        Boid;
+            public TransformMatrix* Transform;
+        }
+
+        unsafe protected override void OnUpdate()
+        {
+            // GetEntities<Group> lets us iterates over all entities
+            // that have both a BoidData and TransformMatrix components attached.
+            foreach (var e in GetEntities<Group>())
+            {
+                var boid = e.Boid;
+                e.Transform->matrix = matrix_math_util.LookRotationToMatrix(boid->position, boid->forward, new float3(0, 1, 0));
+            }
+        }
+    }
+
+    
+    [DisableAutoCreation]
+    class BoidToInstanceRendererTransform_ComponentDataArray : ComponentSystem
     {
         struct Group
         {
-            public ComponentDataArray<BoidData>                  boids;
-            public ComponentDataArray<TransformMatrix> rendererTransforms;
+            [ReadOnly]
+            public ComponentDataArray<BoidData>        Boids;
+            public ComponentDataArray<TransformMatrix> RendererTransforms;
+            public int                                 Length;
+        }
+
+        // [Inject] creates a ComponentGroup, setting up the two ComponentDataArrays so
+        // that we can iterate over all entities containing both BoidData & TransformMatrix.
+        [Inject] 
+        Group m_Group;
+
+        protected override void OnUpdate()
+        {
+            // We iterate over the all injected entities containing BoidData & TransformMatrix Components
+            for (var i = 0; i != m_Group.Length; i++)
+            {
+                var boid = m_Group.Boids[i];
+                
+                TransformMatrix transform;
+                transform.matrix = matrix_math_util.LookRotationToMatrix(boid.position, boid.forward, new float3(0, 1, 0));
+                m_Group.RendererTransforms[i] = transform;
+            }
+        }
+    }
+
+    [DisableAutoCreation]
+    class BoidToInstanceRendererTransform_ParallelForJob_ComponentDataArray  : JobComponentSystem
+    {
+        struct Group
+        {
+            [ReadOnly]
+            public ComponentDataArray<BoidData>        Boids;
+            public ComponentDataArray<TransformMatrix> RendererTransforms;
+            public int                                 Length;
+        }
+
+        // [Inject] creates a ComponentGroup, setting up the two ComponentDataArrays so
+        // that we can iterate over all entities containing both BoidData & TransformMatrix.    
+        [Inject] 
+        Group m_Group;
+        
+        // We use IJobParallelFor to execute the jobs in parallel on multiple cores.
+        [ComputeJobOptimization]
+        struct TransformJob : IJobParallelFor
+        {
+            [ReadOnly]
+            public ComponentDataArray<BoidData>        Boids;
+            public ComponentDataArray<TransformMatrix> TransformMatrices;
+            
+            public void Execute(int index)
+            {
+                var boid = Boids[index];
+                
+                TransformMatrix transform;
+                transform.matrix = matrix_math_util.LookRotationToMatrix(boid.position, boid.forward, new float3(0, 1, 0));
+                
+                TransformMatrices[index] = transform;
+            }
+        }
+        
+        // We derive from JobComponentSystem, the injected m_Group,
+        // Also declares what data is being read & written to in this ComponentSystem.
+        // Because it is declared the JobComponentSystem can give us a Job dependency, which contains all jobs that write to any BoidData or RendererTransforms
+        // We also return the dependency so any scheduled job will now be registered against the types for the next System that might run
+        // This approach means:
+        // * No waiting on main thread, just scheduling jobs with dependencies (Jobs only start when dependencies have completed)
+        // * Dependencies are figured out automatically for us
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var job = new TransformJob() { Boids = m_Group.Boids, TransformMatrices = m_Group.RendererTransforms };
+            return job.Schedule(m_Group.Length, 16, inputDeps);
+        }
+    }
+    
+    // [Inject] creates a ComponentGroup, setting up the two ComponentDataArrays so
+    // that we can iterate over all entities containing both BoidData & TransformMatrix.
+    [DisableAutoCreation]
+    class BoidToInstanceRendererTransform_IJobProcessComponentData : JobComponentSystem
+    {
+        struct Group
+        {
+            [ReadOnly]
+            public ComponentDataArray<BoidData>        Boids;
+            public ComponentDataArray<TransformMatrix> RendererTransforms;
         }
 
         [Inject] 
         Group m_Group;
         
-        [ComputeJobOptimizationAttribute(Accuracy.Med, Support.Relaxed)]
+        // Instead of IJobParallelFor, we use IJobProcessComponentData
+        // It is more efficient than IJobParallelFor and more convenient
+        // * ComponentDataArray has one early out branch per index lookup
+        // * IJobProcessComponentData innerloop does a straight BoidData* array iteration, with zero checks. 
+        [ComputeJobOptimization]
         struct TransformJob : IJobProcessComponentData<BoidData, TransformMatrix>
         {
             public void Execute(ref BoidData boid, ref TransformMatrix transformMatrix)
@@ -37,16 +146,18 @@ namespace BoidSimulations
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
             var job = new TransformJob();
-            return job.Schedule(m_Group.boids, m_Group.rendererTransforms, 16, inputDeps);
+            return job.Schedule(m_Group.Boids, m_Group.RendererTransforms, 16, inputDeps);
         }
     }
 
-#else
 
-    // This code does exactly the same as above.
-    // But using IAutoComponentSystemJob effectively creates the component system code automatically,
-    // so you have to only write the job and scheduling it is taken care of automatically.
-    [ComputeJobOptimizationAttribute(Accuracy.Med, Support.Relaxed)]
+    // We also use IJobProcessComponentData (Same as above)
+    // But we are using IAutoComponentSystemJob.
+    // 
+    // Essentially we tell the ECS that it should create a JobComponentSystem for us that will automatically schedule the job every frame,
+    // 
+    // Effectively it's a simpler way of writing the BoidToInstanceRendererTransform_IJobProcessComponentData
+    [ComputeJobOptimization]
     struct BoidToInstanceRendererTransform : IJobProcessComponentData<BoidData, TransformMatrix>, IAutoComponentSystemJob
     {
         public void Prepare() { }
@@ -56,6 +167,4 @@ namespace BoidSimulations
             transform.matrix = matrix_math_util.LookRotationToMatrix(boid.position, boid.forward, new float3(0, 1, 0));
         }
     }
-    
-#endif
 }

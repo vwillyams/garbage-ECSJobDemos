68851e8a4302409f5048ecfc5f2891599fead2af
Daniel Brauer <danielbrauer@unity3d.com>
2018-02-01 19:31:41 +0100

    Updated hybrid version

---
+++ ECSJobDemos/Assets/ECS/TwoStickShooterHybrid/Components/EnemyShootState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwoStickHybridExample
+{
+
+    public class EnemyShootState : MonoBehaviour
+    {
+
+        public float Cooldown;
+    }
+}
--- ECSJobDemos/Assets/ECS/TwoStickShooterHybrid/TwoStickExampleSettings.cs
+++ ECSJobDemos/Assets/ECS/TwoStickShooterHybrid/TwoStickExampleSettings.cs
@@ -5,14 +5,18 @@ namespace TwoStickHybridExample
     public class TwoStickExampleSettings : MonoBehaviour
     {
         public float playerMoveSpeed = 15.0f;
-        public float bulletMoveSpeed = 30.0f;
-        public float bulletTimeToLive = 2.0f;
         public float playerFireCoolDown = 0.1f;
         public float enemySpeed = 8.0f;
+        public float enemyShootRate = 1.0f;
+        public float playerCollisionRadius = 1.0f;
+        public float enemyCollisionRadius = 1.0f;
+        public Rect playfield = new Rect { x = -30.0f, y = -30.0f, width = 60.0f, height = 60.0f }; 
 
-        public Shot ShotPrefab;
+        public Shot PlayerShotPrefab;
+        public Shot EnemyShotPrefab;
         public Transform2D PlayerPrefab;
-        public Enemy EnemyPrefab;
+        public Transform2D EnemyPrefab;
         public EnemySpawnSystemState EnemySpawnState;
+        public Faction EnemyFaction;
     }
 }

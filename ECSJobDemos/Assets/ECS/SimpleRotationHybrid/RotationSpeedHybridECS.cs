3ced1682a7b0339b2b844b5de545de3ac1fca16b
joeante <joe@unity3d.com>
2018-02-03 14:53:53 +0100

    SimpleRotationHybrid

---
+++ ECSJobDemos/Assets/ECS/SimpleRotationHybrid/RotationSpeedHybridECS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.ECS;
+
+namespace RotatorSamples
+{
+	public class RotationSpeedHybridECS : MonoBehaviour
+	{
+		public float speed;
+	}
+
+	public class RotatingSystemHybridECS : ComponentSystem
+	{
+		struct Group
+		{
+			public Transform 				transform;
+			public RotationSpeedHybridECS   rotation;
+		}
+
+		protected override void OnUpdate()
+		{
+			float dt = Time.deltaTime;
+			foreach(var e in GetEntities<Group>())
+			{
+				e.transform.rotation = e.transform.rotation * Quaternion.AngleAxis(dt * e.rotation.speed, Vector3.up);
+			}
+		}
+	}
+}
+

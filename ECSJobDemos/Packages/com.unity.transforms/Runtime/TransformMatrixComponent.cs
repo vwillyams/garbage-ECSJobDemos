3bfbd0e89064bc977e7b8ff7aab505420edc888b
Mike Acton <macton@gmail.com>
2018-02-18 21:44:25 -0800

    Documentation

--- ECSJobDemos/Packages/com.unity.transforms/Runtime/TransformMatrixComponent.cs
+++ ECSJobDemos/Packages/com.unity.transforms/Runtime/TransformMatrixComponent.cs
@@ -3,6 +3,10 @@ using Unity.Mathematics;
 
 namespace Unity.Transforms
 {
+    /// <summary>
+    /// Store (calculated) object to world matrix. 
+    /// Required by other systems. e.g. MeshInstanceRenderer
+    /// </summary>
     public struct TransformMatrix : IComponentData
     {
         public float4x4 Value;
--- ECSJobDemos/Packages/com.unity.transforms/Runtime/TransformParentComponent.cs
+++ ECSJobDemos/Packages/com.unity.transforms/Runtime/TransformParentComponent.cs
@@ -2,6 +2,13 @@
 
 namespace Unity.Transforms
 {
+    /// <summary>
+    /// User assigned parent Entity.
+    /// Local transformations (e.g. LocalPosition, LocalRotation) will be relative
+    /// to the object to world matrix associated with the parent entity.
+    /// The parent's object to world matrix may only be temporary if it's not explicitly
+    /// stored in a TransformMatrix.
+    /// </summary>
     public struct TransformParent : IComponentData
     {
         public Entity Value;

bc611727b7bc0f56df724c211ce2de59edc36f21
Daniel Brauer <danielbrauer@unity3d.com>
2018-02-19 17:42:26 +0100

    Empty window

---
+++ ECSJobDemos/Packages/com.unity.ecs/Editor/ECSDebugger/ECSDebugger.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Unity.ECS;
+using UnityEngine;
+using UnityEditor.IMGUI.Controls;
+
+namespace UnityEditor.ECS
+{
+    public class ECSDebugger : EditorWindow {
+
+        [MenuItem("Window/ECS Debugger", false, 2017)]
+        static void OpenWindow()
+        {
+            GetWindow<ECSDebugger>("ECS Debugger");
+        }
+
+        [SerializeField]
+        private TreeViewState systemListState;
+        
+        private SystemListView systemListView;
+
+        void OnEnable()
+        {
+//            systemListView = new SystemListView(systemListState, );
+        }
+
+        void OnGUI()
+        {
+            GUILayout.Label("Systems", EditorStyles.boldLabel);
+            
+            
+            
+            GUILayout.BeginVertical();
+            GUI.Label(GUIHelpers.GetExpandingRect(), "", GUI.skin.box);
+            
+            GUILayout.EndVertical();
+        }
+    }
+}

c061b67af62af6bceb5fbc1d6057a7ead65ee267
joeante <joe@unity3d.com>
2018-02-21 03:35:51 +0100

    Fast equality support

--- ECSJobDemos/Packages/com.unity.ecs/Runtime/Types/FastEquality.cs
+++ ECSJobDemos/Packages/com.unity.ecs/Runtime/Types/FastEquality.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using Boo.Lang;
 using Unity.Collections.LowLevel.Unsafe;
 
@@ -7,13 +8,18 @@ using Unity.Collections.LowLevel.Unsafe;
 
 namespace Unity.ECS
 {
-    static internal class AlignmentLayout
+    static internal class FastEquality
     {
         internal struct Layout
         {
             public int  offset;
             public int  count;
             public bool Aligned4;
+
+            public override string ToString()
+            {
+                return $"offset: {offset} count: {count} Aligned4: {Aligned4}";
+            }
         }
         
         
@@ -29,9 +35,23 @@ namespace Unity.ECS
             if (begin != end)
                 layouts.Add(new Layout {offset = begin, count = end - begin, Aligned4 = false});
 
-            //@TODO: Support align4 optimization
-            
-            return layouts.ToArray();
+            var layoutsArray = layouts.ToArray();
+
+            for (int i = 0; i != layoutsArray.Length; i++)
+            {
+                if (layoutsArray[i].count % 4 == 0 && layoutsArray[i].offset % 4 == 0)
+                {
+                   layoutsArray[i].count /= 4;
+                   layoutsArray[i].Aligned4 = true;
+                }
+            }
+
+            return layoutsArray;
+        }
+
+        unsafe struct PointerSize
+        {
+            void* pter;
         }
 
         static void CreateLayoutRecurse(Type type, int baseOffset, List<Layout> layouts, ref int begin, ref int end)
@@ -42,9 +62,14 @@ namespace Unity.ECS
             {
                 int offset = baseOffset + UnsafeUtility.GetFieldOffset(field);
 
-                if (field.FieldType.IsPrimitive || field.FieldType.IsPointer)
+                if (field.FieldType.IsPrimitive || field.FieldType.IsPointer || field.FieldType.IsClass)
                 {
-                    int sizeOf  = UnsafeUtility.SizeOf(field.FieldType);
+                    int sizeOf = -1;
+                    if (field.FieldType.IsPointer || field.FieldType.IsClass)
+                        sizeOf = UnsafeUtility.SizeOf<PointerSize>();
+                    else
+                        sizeOf = UnsafeUtility.SizeOf(field.FieldType);
+                        
                     if (end != offset)
                     {
                         layouts.Add(new Layout {offset = begin, count = end - begin, Aligned4 = false});
@@ -63,10 +88,23 @@ namespace Unity.ECS
             }
         }
 
+        //@TODO: Encode type in hashcode...
+        
         const int FNV_32_PRIME = 0x01000193;
 
-        unsafe static int GetHashCode(byte* data, Layout[] layout)
+        unsafe public static int GetHashCode<T>(T lhs, Layout[] layout) where T : struct
         {
+            return GetHashCode(UnsafeUtility.AddressOf(ref lhs), layout);
+        }
+
+        unsafe public static int GetHashCode<T>(ref T lhs, Layout[] layout) where T : struct
+        {
+            return GetHashCode(UnsafeUtility.AddressOf(ref lhs), layout);
+        }
+
+        unsafe public static int GetHashCode(void* dataPtr, Layout[] layout)
+        {
+            byte* data = (byte*)dataPtr;
             uint hash = 0;
             
             for (int k = 0; k != layout.Length; k++)
@@ -75,7 +113,7 @@ namespace Unity.ECS
                 {
                     uint* dataInt = (uint*)(data + layout[k].offset);
                     int count = layout[k].count;
-                    for (int i = 0; i != count; k++)
+                    for (int i = 0; i != count; i++)
                     {
                         hash *= FNV_32_PRIME;
                         hash ^= dataInt[i];
@@ -85,7 +123,7 @@ namespace Unity.ECS
                 {
                     byte* dataByte = data + layout[k].offset;
                     int count = layout[k].count;
-                    for (int i = 0; i != count; k++)
+                    for (int i = 0; i != count; i++)
                     {
                         hash *= FNV_32_PRIME;
                         hash ^= (uint)dataByte[i];
@@ -96,27 +134,42 @@ namespace Unity.ECS
             return (int)hash;
         }
         
-        unsafe static bool Equals(byte* lhs, byte* rhs, Layout[] layout)
+        unsafe public static bool Equals<T>(T lhs, T rhs, Layout[] layout) where T : struct
         {
+            return Equals(UnsafeUtility.AddressOf(ref lhs), UnsafeUtility.AddressOf(ref rhs), layout);
+        }
+
+        unsafe public static bool Equals<T>(ref T lhs, ref T rhs, Layout[] layout) where T : struct
+        {
+            return Equals(UnsafeUtility.AddressOf(ref lhs), UnsafeUtility.AddressOf(ref rhs), layout);
+        }
+        
+        unsafe public static bool Equals(void* lhsPtr, void* rhsPtr, Layout[] layout)
+        {
+            byte* lhs = (byte*)lhsPtr;
+            byte* rhs = (byte*)rhsPtr;
+            
             bool same = true;
             
             for (int k = 0; k != layout.Length; k++)
             {
                 if (layout[k].Aligned4)
                 {
-                    uint* lhsInt = (uint*)(lhs + layout[k].offset);
-                    uint* rhsInt = (uint*)(rhs + layout[k].offset);
+                    int offset = layout[k].offset;
+                    uint* lhsInt = (uint*)(lhs + offset);
+                    uint* rhsInt = (uint*)(rhs + offset);
                     int count = layout[k].count;
-                    for (int i = 0; i != count; k++)
-                        same &= lhs[i] != rhs[i];
+                    for (int i = 0; i != count; i++)
+                        same &= lhs[i] == rhs[i];
                 }
                 else
                 {
-                    byte* lhsByte = lhs + layout[k].offset;
-                    byte* rhsByte = rhs + layout[k].offset;
+                    int offset = layout[k].offset;
+                    byte* lhsByte = lhs + offset;
+                    byte* rhsByte = rhs + offset;
                     int count = layout[k].count;
-                    for (int i = 0; i != count; k++)
-                        same &= lhs[i] != rhs[i];
+                    for (int i = 0; i != count; i++)
+                        same &= lhs[i] == rhs[i];
                 }
             }
 
--- ECSJobDemos/Packages/com.unity.ecs/Tests/Runtime/FastEqualityTests.cs
+++ ECSJobDemos/Packages/com.unity.ecs/Tests/Runtime/FastEqualityTests.cs
@@ -2,21 +2,33 @@
 using NUnit.Framework;
 using Unity.ECS;
 using Unity.Mathematics;
+using Unity.Collections.LowLevel.Unsafe;
 
 namespace UnityEngine.ECS.Tests
 {
     public class FastEqualityTests
     {
+        [StructLayout(LayoutKind.Sequential)]
         struct Simple
         {
             int a;
             int b;
         }
 
+        [StructLayout(LayoutKind.Sequential)]
         struct SimpleEmbedded
         {
             float4 a;
             int b;
+        }
+        
+        [StructLayout(LayoutKind.Sequential)]
+
+        struct BytePadding
+        {
+            byte a;
+            byte b;
+            float c;
         }
         
         [StructLayout(LayoutKind.Sequential)]
@@ -44,66 +56,92 @@ namespace UnityEngine.ECS.Tests
         {
             string blah;
         }
-        
+
+
+        int PtrAligned4Count = UnsafeUtility.SizeOf<FloatPointer>() / 4;
         
         [Test]
         public void SimpleLayout()
         {
-            var res = AlignmentLayout.CreateLayout(typeof(Simple));
+            var res = FastEquality.CreateLayout(typeof(Simple));
             Assert.AreEqual(1, res.Length);
-            Assert.AreEqual(0, res[0].offset);
-            Assert.AreEqual(8, res[0].count);
+            Assert.AreEqual(new FastEquality.Layout {offset = 0, count = 2, Aligned4 = true }, res[0]);
+            
+            
         }
 
         [Test]
-        [Ignore("Fails")]
         public void PtrLayout()
         {
-            var res = AlignmentLayout.CreateLayout(typeof(FloatPointer));
-            Assert.AreEqual(1, res.Length);
-            Assert.AreEqual(0, res[0].offset);
-            Assert.AreEqual(8, res[0].count);
+            var layout = FastEquality.CreateLayout(typeof(FloatPointer));
+            Assert.AreEqual(1, layout.Length);
+            Assert.AreEqual(new FastEquality.Layout {offset = 0, count = PtrAligned4Count, Aligned4 = true }, layout[0]);
         }
         
         [Test]
-        [Ignore("Fails")]
         public void ClassLayout()
         {
-            var res = AlignmentLayout.CreateLayout(typeof(ClassInStruct));
-            Assert.AreEqual(1, res.Length);
-            Assert.AreEqual(0, res[0].offset);
-            Assert.AreEqual(8, res[0].count);
+            var layout = FastEquality.CreateLayout(typeof(ClassInStruct));
+            Assert.AreEqual(1, layout.Length);
+            Assert.AreEqual(new FastEquality.Layout {offset = 0, count = PtrAligned4Count, Aligned4 = true }, layout[0]);
         }
         
         [Test]
         public void SimpleEmbeddedLayout()
         {
-            var res = AlignmentLayout.CreateLayout(typeof(SimpleEmbedded));
-            Assert.AreEqual(1, res.Length);
-            Assert.AreEqual(0, res[0].offset);
-            Assert.AreEqual(20, res[0].count);
+            var layout = FastEquality.CreateLayout(typeof(SimpleEmbedded));
+            Assert.AreEqual(1, layout.Length);
+            Assert.AreEqual(new FastEquality.Layout {offset = 0, count = 5, Aligned4 = true }, layout[0]);
         }
         
         [Test]
         public void EndPaddingLayout()
         {
-            var res = AlignmentLayout.CreateLayout(typeof(EndPadding));
-            Assert.AreEqual(1, res.Length);
-            Assert.AreEqual(0, res[0].offset);
-            Assert.AreEqual(12, res[0].count);
+            var layout = FastEquality.CreateLayout(typeof(EndPadding));
+            Assert.AreEqual(1, layout.Length);
+            Assert.AreEqual(new FastEquality.Layout {offset = 0, count = 3, Aligned4 = true }, layout[0]);
         }
         
         [Test]
         public void AlignSplitLayout()
         {
-            var res = AlignmentLayout.CreateLayout(typeof(AlignSplit));
-            Assert.AreEqual(2, res.Length);
+            var layout = FastEquality.CreateLayout(typeof(AlignSplit));
+            Assert.AreEqual(2, layout.Length);
             
-            Assert.AreEqual(0, res[0].offset);
-            Assert.AreEqual(12, res[0].count);
+            Assert.AreEqual(new FastEquality.Layout {offset = 0, count = 3, Aligned4 = true }, layout[0]);
+            Assert.AreEqual(new FastEquality.Layout {offset = 16, count = 2, Aligned4 = true }, layout[1]);
+        }
+        
+        [Test]
+        public void BytePaddding()
+        {
+            var layout = FastEquality.CreateLayout(typeof(BytePadding));
+            Assert.AreEqual(2, layout.Length);
             
-            Assert.AreEqual(16, res[1].offset);
-            Assert.AreEqual(8, res[1].count);
+            Assert.AreEqual(new FastEquality.Layout {offset = 0, count = 2, Aligned4 = false }, layout[0]);
+            Assert.AreEqual(new FastEquality.Layout {offset = 4, count = 1, Aligned4 = true }, layout[1]);
+        }
+        
+        [Test]
+        public void EqualsInt4()
+        {
+            var layout = FastEquality.CreateLayout(typeof(int4));
+                        
+            Assert.IsTrue(FastEquality.Equals(new int4(1, 2, 3, 4), new int4(1, 2, 3, 4), layout));
+            Assert.IsTrue(FastEquality.Equals(new int4(1, 2, 3, 4), new int4(1, 2, 3, 5), layout));
+        }
+        
+        [Test]
+        unsafe public void GetHashCodeInt4()
+        {
+            var layout = FastEquality.CreateLayout(typeof(int4));
+            Assert.AreEqual(-270419516, FastEquality.GetHashCode(new int4(1, 2, 3, 4), layout));
+            Assert.AreEqual(-270419517, FastEquality.GetHashCode(new int4(1, 2, 3, 3), layout));
+            Assert.AreEqual(1, FastEquality.GetHashCode(new int4(0, 0, 0, 1), layout));
+            Assert.AreEqual(16777619, FastEquality.GetHashCode(new int4(0, 0, 1, 0), layout));
+            Assert.AreEqual(0, FastEquality.GetHashCode(new int4(0, 0, 0, 0), layout));
+
+            // Note, builtin .GetHashCode() returns different values even for all zeros...
         }
     }
 }

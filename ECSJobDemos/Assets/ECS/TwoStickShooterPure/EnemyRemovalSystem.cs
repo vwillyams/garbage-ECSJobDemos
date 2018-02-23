99c64112493a6fa2f92714ad0e0174dfed3095e9
Andreas Fredriksson <andreasf@unity3d.com>
2018-02-23 12:48:51 -0800

    Renaming

---
+++ ECSJobDemos/Assets/ECS/TwoStickShooterPure/EnemyRemovalSystem.cs
@@ -0,0 +1,56 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms2D;
+
+namespace TwoStickPureExample
+{
+    // Removes enemies that are off screen
+    class EnemyRemovalSystem : JobComponentSystem
+    {
+        public struct Data
+        {
+            public int Length;
+            [ReadOnly] public ComponentDataArray<Enemy> EnemyTag;
+            public ComponentDataArray<Health> Health;
+            [ReadOnly] public ComponentDataArray<Position2D> Position;
+        }
+
+        [Inject] Data m_Data;
+
+        public struct BoundaryKillJob : IJobParallelFor
+        {
+            public ComponentDataArray<Health> Health;
+            [ReadOnly] public ComponentDataArray<Position2D> Position;
+
+            public float MinY;
+            public float MaxY;
+
+            public void Execute(int index)
+            {
+                var position = Position[index].Value;
+
+                if (position.y > MaxY || position.y < MinY)
+                {
+                    Health[index] = new Health { Value = -1.0f };
+                }
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            if (!TwoStickBootstrap.Settings)
+                return inputDeps;
+
+            var boundaryKillJob = new BoundaryKillJob
+            {
+                Health = m_Data.Health,
+                Position = m_Data.Position,
+                MinY = TwoStickBootstrap.Settings.playfield.yMin,
+                MaxY = TwoStickBootstrap.Settings.playfield.yMax,
+            };
+
+            return boundaryKillJob.Schedule(m_Data.Length, 64, inputDeps);
+        }
+    }
+}

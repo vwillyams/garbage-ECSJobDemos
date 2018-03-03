d252a9a0cd60a0d24241ef23be3c69f73d5cb29e
joeante <joe@unity3d.com>
2018-03-04 00:07:39 +0100

    Initial pass on supporting ref returns in IJobProcessComponentData

---
+++ ECSJobDemos/Packages/com.unity.collections/Unity.Collections/UnsafeUtilityEx.cs
@@ -0,0 +1,17 @@
+﻿#if CSHARP_7_OR_LATER
+namespace Unity.Collections.LowLevel.Unsafe
+{
+    unsafe public static class UnsafeUtilityEx
+    {
+        public static ref T AsRef<T>(void* ptr) where T : struct
+        {
+            return ref System.Runtime.CompilerServices.Unsafe.AsRef<T>(ptr);
+        }
+    
+        public static ref T ArrayElementAsRef<T>(void* ptr, int index) where T : struct
+        {
+            return ref System.Runtime.CompilerServices.Unsafe.AsRef<T>((byte*)ptr + index * UnsafeUtility.SizeOf<T>());
+        }
+    }
+}
+#endif

6ca3acbf1adfc12da0dee05707de016803cb2f6d
joeante <joe@unity3d.com>
2018-03-06 22:30:23 +0100

    SubtractiveComponent also supports all component types now

--- ECSJobDemos/Packages/com.unity.entities/Unity.Entities.Tests/CreateAndDestroyTests.cs
+++ ECSJobDemos/Packages/com.unity.entities/Unity.Entities.Tests/CreateAndDestroyTests.cs
@@ -212,23 +212,6 @@ namespace UnityEngine.ECS.Tests
 
 			m_Manager.AddComponentData<EcsTestData>(entity, new EcsTestData());
 			Assert.AreEqual(0, subtractiveArch.GetComponentDataArray<EcsTestData2>().Length);
-
-			m_Manager.DestroyEntity(entity);
 		}
 	}
-/*
-	public class RefReturnsTests
-	{
-		[Test]
-		public void TestRefReturns()
-		{
-			IntPtr memory = UnsafeUtility.Malloc(1000, 4, Allocator.Persistent);
-
-			var value = UnsafeUtility.RefArrayElementWithStride<float>(memory, 0, 4);
-			value = 5;
-
-			Assert.AreEqual(5, UnsafeUtility.ReadArrayElementWithStride<float>(memory, 0, 4));
-		}
-	}
-*/
 }
--- ECSJobDemos/Packages/com.unity.entities/Unity.Entities.Tests/InjectComponentGroupTests.cs
+++ ECSJobDemos/Packages/com.unity.entities/Unity.Entities.Tests/InjectComponentGroupTests.cs
@@ -51,7 +51,8 @@ namespace UnityEngine.ECS.Tests
 			public struct Datas
 			{
 				public ComponentDataArray<EcsTestData> Data;
-				public SubtractiveComponent<EcsTestData2> Data2;
+			    public SubtractiveComponent<EcsTestData2> Data2;
+			    public SubtractiveComponent<Rigidbody> Rigidbody;
 			}
 
 			[Inject]
@@ -105,15 +106,28 @@ namespace UnityEngine.ECS.Tests
         {
             var subtractiveSystem = World.GetOrCreateManager<SubtractiveSystem> ();
 
-            var go = m_Manager.CreateEntity (new ComponentType[0]);
-            m_Manager.AddComponentData (go, new EcsTestData(2));
+            var entity = m_Manager.CreateEntity (typeof(EcsTestData));
 
+            var go = new GameObject("Test", typeof(EcsTestComponent));
+            go.GetComponent<GameObjectEntity>().OnEnable();
+            
+            // Ensure entities without the subtractive components are present
             subtractiveSystem.Update ();
-            Assert.AreEqual (1, subtractiveSystem.Group.Data.Length);
-            Assert.AreEqual (2, subtractiveSystem.Group.Data[0].value);
-            m_Manager.AddComponentData (go, new EcsTestData2());
+            Assert.AreEqual (2, subtractiveSystem.Group.Data.Length);
+            Assert.AreEqual (0, subtractiveSystem.Group.Data[0]);
+            Assert.AreEqual (0, subtractiveSystem.Group.Data[1]);
+            
+            // Ensure adding the subtractive components, removes them from the injection
+            m_Manager.AddComponentData (entity, new EcsTestData2());
+            
+            // TODO: This should be automatic...
+            go.AddComponent<Rigidbody>();
+            go.GetComponent<GameObjectEntity>().OnDisable(); go.GetComponent<GameObjectEntity>().OnEnable();
+            
             subtractiveSystem.Update ();
             Assert.AreEqual (0, subtractiveSystem.Group.Data.Length);
+
+            Object.DestroyImmediate(go);
         }
 
 	    [Test]

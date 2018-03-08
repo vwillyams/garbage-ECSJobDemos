ee78b625282c9d5030f3b1be21c1487fd663d571
joeante <joe@unity3d.com>
2018-03-08 09:26:45 -0800

    Simplify RandomInitialHeadingSystem

--- ECSJobDemos/Assets/GameCode/SimpleRotation/RandomIInitialHeadingSystem.cs
+++ ECSJobDemos/Assets/GameCode/SimpleRotation/RandomIInitialHeadingSystem.cs
@@ -5,40 +5,31 @@ using Unity.Jobs;
 
 namespace UnityEngine.ECS.SimpleRotation
 {
-    public class RandomInitialHeadingSystem : JobComponentSystem
+    public class RandomInitialHeadingSystem : ComponentSystem
     {
         struct RandomInitialHeadingGroup
         {
-            public ComponentDataArray<Heading> headings;
-            [ReadOnly] public ComponentDataArray<RandomInitialHeading> randomInitialHeadiings;
-            public EntityArray entities;
+            public ComponentDataArray<Heading> Headings;
+            [ReadOnly] public ComponentDataArray<RandomInitialHeading> RandomInitialHeadiings;
+            public EntityArray Entities;
             public int Length;
         }
 
-        [Inject] private RandomInitialHeadingGroup m_RandomInitialHeadingGroup;
-        [Inject] private EntityManager m_EntityManager;
+        [Inject] RandomInitialHeadingGroup m_Group;
 
-        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        protected override void OnUpdate()
         {
-            if (m_RandomInitialHeadingGroup.Length == 0)
-                return inputDeps;
-
-            inputDeps.Complete();
-            var entities = new NativeArray<Entity>(m_RandomInitialHeadingGroup.Length, Allocator.Temp);
-            for (int i = 0; i < m_RandomInitialHeadingGroup.Length; i++)
+            var entities = new NativeArray<Entity>(m_Group.Length, Allocator.Temp);
+            for (int i = 0; i < m_Group.Length; i++)
             {
-                entities[i] = m_RandomInitialHeadingGroup.entities[i];
-                m_RandomInitialHeadingGroup.headings[i] = new Heading
+                entities[i] = m_Group.Entities[i];
+                m_Group.Headings[i] = new Heading
                 {
                     Value = math.normalize(new float3(Random.Range(-1, 1), Random.Range(-1,1), Random.Range(-1, 1)))
                 };
+                
+                PostUpdateCommands.RemoveComponent<RandomInitialHeading>(m_Group.Entities[i]);
             }
-            for (int i = 0; i < entities.Length; i++)
-            {
-                EntityManager.RemoveComponent<RandomInitialHeading>(entities[i]);
-            }
-            entities.Dispose();
-            return new JobHandle();
         }
     }
 }

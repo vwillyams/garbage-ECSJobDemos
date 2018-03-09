c2d5d4f526f9e6697c08b137310d58319ccbd13d
Henrik Poulsen <henrikp@unity3d.com>
2018-03-09 09:59:13 +0100

    Removed unused NativeArray

--- ECSJobDemos/Assets/GameCode/SimpleRotation/RandomIInitialHeadingSystem.cs
+++ ECSJobDemos/Assets/GameCode/SimpleRotation/RandomIInitialHeadingSystem.cs
@@ -19,10 +19,8 @@ namespace UnityEngine.ECS.SimpleRotation
 
         protected override void OnUpdate()
         {
-            var entities = new NativeArray<Entity>(m_Group.Length, Allocator.Temp);
             for (int i = 0; i < m_Group.Length; i++)
             {
-                entities[i] = m_Group.Entities[i];
                 m_Group.Headings[i] = new Heading
                 {
                     Value = math.normalize(new float3(Random.Range(-1, 1), Random.Range(-1,1), Random.Range(-1, 1)))

c73733f4cb40e04d936d51d5c7575303df7e40cd
Henrik Poulsen <henrikp@unity3d.com>
2018-03-12 15:37:43 +0100

    Initial work on adapting the publishStable script for ci

---
+++ publishStable.py
@@ -0,0 +1,216 @@
+import filecmp
+import glob
+import os
+import subprocess
+import shutil
+
+dry_run = True
+dest_repo = "moo"
+dest_branch = "blah"
+view_registries = ["https://packages.unity.com", "https://staging-packages.unity.com", "http://10.45.32.202:4873"]
+best_view_registry = None
+publish_registry = "http://10.45.32.202:4873"
+
+
+def get_packages_folder():
+    # type: () -> str
+    path = "ECSJobDemos/Packages/"
+    if os.path.isfile(path + "manifest.json"):
+        return os.path.abspath(path)
+    raise Exception("Unable to find {0}".format(path))
+
+
+def get_list_of_packages(folder):
+    # type: (str) -> [str]
+    packages = []
+    for f in glob.glob(os.path.join(folder, "**/package.json")):
+        packages.append(os.path.dirname(f))
+    return packages
+
+
+def is_package_changed(package_folder, package_name):
+    # type: (str) -> bool
+    print "Installing {0} from {1} to see if we have changed anything".format(package_name, best_view_registry)
+    npm_cmd("install --no-package-lock --prefix . {0}".format(package_name), best_view_registry, False)
+    installed_path = os.path.join("node_modules", package_name)
+    print "Comparing files between {0} and {1}".format(installed_path, package_folder)
+    match, mismatch, errors = filecmp.cmpfiles(installed_path, package_folder, [])
+    shutil.rmtree("node_modules")
+    if os.path.isdir("etc"):
+        shutil.rmtree("etc")
+    if len(mismatch) == 0 and len(errors) == 0:
+        print "Nothing has changed"
+        return False
+    print "Content has changed"
+    return True
+    pass
+
+
+def get_next_package_version(package_name, major, minor, patch):
+    # type: (str) -> str
+
+    global best_view_registry
+    print "Getting current published package version for {0}".format(package_name)
+    highest_version = "0.0.0"
+
+    highest_version_trimmed = highest_version.split('.')
+    for registry in view_registries:
+        try:
+            version = npm_cmd("view {0} version".format(package_name), registry, False)
+
+        # Hack for sinopia which returns 404 when a package doesn't exist, whereas no other registry does
+        except subprocess.CalledProcessError as e:
+            if "is not in the npm registry" in e.output:
+                version = ""
+            else:
+                raise e
+        if not version:
+            # Package didn't exist in the registry
+            continue
+        trimmed_version = version.strip().split('+')[0].split('.')
+        for i in range(0, 3):
+            if int(trimmed_version[i]) > int(highest_version_trimmed[i]):
+                highest_version = version.strip()
+                highest_version_trimmed = trimmed_version
+                best_view_registry = registry
+
+    print "{0} was selected as the best registry to ready from since it had the highest package version for {1} ({2})".format(
+        best_view_registry, package_name, highest_version)
+
+    new_major = int(highest_version_trimmed[0])
+    new_minor = int(highest_version_trimmed[1])
+    new_patch = int(highest_version_trimmed[2])
+    if major:
+        new_major += 1
+    if minor:
+        new_minor += 1
+    if patch:
+        new_patch += 1
+    new_version = "{0}.{1}.{2}".format(new_major, new_minor, new_patch)
+    print "New version is changed from {0} to {1}".format(highest_version, new_version)
+
+    return new_version
+
+
+def publish_new_package(package_path, version):
+    # type: () -> str
+    previous_cwd = os.getcwd()
+    try:
+
+        os.chdir(package_path)
+        npm_cmd("--no-git-tag-version version {0}".format(version), None, False).strip()
+        print "Packing as version {0}".format(version)
+        package_archive = npm_cmd("pack .", None, False).strip()
+        print "Publishing {0} to {1}".format(package_archive, publish_registry)
+        npm_cmd("publish {0}".format(package_archive), publish_registry, True)
+        os.remove(package_archive)
+    finally:
+        os.chdir(previous_cwd)
+    pass
+
+
+def modify_manifest(name, version):
+    pass
+
+
+def scatter_manifest():
+    pass
+
+
+def squash_commits():
+    # type: () -> str
+    pass
+
+
+def remove_this_script_from_commit():
+    pass
+
+
+def git_cmd(cmd, is_destructive=True):
+    formatted_cmd = "git {0}".format(cmd)
+    if is_destructive and dry_run:
+        print "  Skipping: {0}".format(formatted_cmd)
+        return None
+    print cmd
+    return ""
+
+
+def npm_cmd(cmd, registry, is_destructive=True):
+    registry_cmd = ''
+    if registry:
+        registry_cmd = "--registry {0}".format(registry)
+
+    formatted_cmd = 'npm {0} {1}'.format(cmd, registry_cmd)
+    if is_destructive and dry_run:
+        print "  Skipping: {0}".format(formatted_cmd)
+        return None
+
+    print "  Running: {0}".format(formatted_cmd)
+    return subprocess.check_output(formatted_cmd, shell=True, stderr=subprocess.STDOUT)
+
+
+def main():
+    root_dir = os.getcwd()
+    try:
+        print "Will now start creating packages for publish from $srcRepo to $dstRepo"
+        root_clone = os.getcwd()
+        print "Adding $dstRepo as remote to repo"
+        git_cmd("remote add stable {0}".format(dest_repo), True)
+        print "Removing $srcRepo from remote of repo"
+        git_cmd("remote remove origin", True)
+        release_number = squash_commits()
+        packages_folder = get_packages_folder()
+        packages = get_list_of_packages(packages_folder)
+
+        for package_path in packages:
+            package_name = os.path.basename(os.path.normpath(package_path))
+            print "### Package Found: {0} in {1}".format(package_name, package_path).ljust(80, '#')
+            package_version = get_next_package_version(package_name, False, False, True)
+
+            changed = is_package_changed(package_path, package_name)
+            if changed is True:
+                print "The package repository is not clean. A new version of $package will be published"
+                package_version = publish_new_package(package_path)
+            else:
+                publish_new_package(package_path, package_version)
+                print "No change detected in the repo. The version {0} will be used in the project".format(
+                    package_version)
+
+            os.chdir(packages_folder)
+            if not dry_run:
+                shutil.rmtree(package_path)
+
+            modify_manifest(package_path, package_version)
+
+            os.chdir(root_clone)
+            git_cmd("add .", True)
+            print ''.ljust(80, '#')
+
+        scatter_manifest()
+
+        remove_this_script_from_commit()
+        if release_number != 1:
+            print "Committing changes with release number {0}".format(release_number)
+            git_cmd("commit -m \"Release {0}\"".format(release_number), True)
+        else:
+            print "Amending first commit with release number {0}".format(release_number)
+            git_cmd("commit --amend --reset -author -m \"Release {0}\"".format(release_number), True)
+            pass
+        print "Pushing squashed branch {0} to remote stable".format(dest_branch)
+        git_cmd("push stable {0}".format(dest_branch), True)
+        os.chdir(root_dir)
+    except subprocess.CalledProcessError as e:
+        print e
+        print e.output
+        raise
+    finally:
+        os.chdir(root_dir)
+        # TODO restore has been modified during a dry run
+        if dry_run:
+            git_cmd("reset --hard HEAD")
+            git_cmd("git clean -f -d")
+        pass
+
+
+if __name__ == "__main__":
+    main()

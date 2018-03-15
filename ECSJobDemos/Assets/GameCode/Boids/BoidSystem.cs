e20988f90ec9c39e48c86ea500d2c6ef167a4160
Mike Acton <macton@gmail.com>
2018-03-15 10:06:08 -0700

    Simplify. Move obstacle/target tracking to cell.

--- ECSJobDemos/Assets/GameCode/Boids/BoidSystem.cs
+++ ECSJobDemos/Assets/GameCode/Boids/BoidSystem.cs
@@ -32,6 +32,9 @@ namespace Samples.Boids
             public float3 alignment;
             public float3 separation;
             public float  count;
+            public float3 obstaclePosition;
+            public float  obstacleDistance;
+            public float3 targetPosition;
         }
 
         [ComputeJobOptimization]
@@ -50,8 +53,8 @@ namespace Samples.Boids
                 hashMap.Add(hash, index);
                 cells[index] = new Cell
                 {
-                    count = 1,
-                    alignment = headings[index].Value,
+                    count      = 1,
+                    alignment  = headings[index].Value,
                     separation = -position
                 };
             }
@@ -62,41 +65,12 @@ namespace Samples.Boids
         {
             // Anything with the same firstIndex is gauranteed to be running on the same thread, 
             // so there is not ParallelFor concurrency issue.
-            [NativeDisableParallelForRestriction] public NativeArray<Cell> cells;
-            [NativeDisableParallelForRestriction] public NativeArray<int>  cellIndices;
-
-            public void Execute(int firstIndex, int index)
-            {
-                cellIndices[index] = firstIndex;
-                if (firstIndex != index)
-                {
-                    cells[firstIndex] = new Cell
-                    {
-                        count      = cells[firstIndex].count + 1.0f,
-                        alignment  = cells[firstIndex].alignment + cells[index].alignment,
-                        separation = cells[firstIndex].separation + cells[index].separation
-                    };
-                }
-            }
-        }
-
-        [ComputeJobOptimization]
-        struct Steer : IJobParallelFor
-        {
-            [ReadOnly] public NativeArray<int>         cellIndices;
-            [ReadOnly] public NativeArray<Cell>        cells;
-            [ReadOnly] public NativeArray<Position>    targetPositions;
-            [ReadOnly] public NativeArray<Position>    obstaclePositions;
-            [ReadOnly] public Boid                     settings;
-            public float3                              dt;
-            public float3                              alignmentWeight;
-            public float3                              separationWeight;
-            public float3                              speed;
-            public ComponentDataArray<Position>        positions;
-            public ComponentDataArray<Heading>         headings;
-            public ComponentDataArray<TransformMatrix> transformMatrices;
+            [NativeDisableParallelForRestriction] public NativeArray<Cell>                       cells;
+            [NativeDisableParallelForRestriction] public NativeArray<int>                        cellIndices;
+            [NativeDisableParallelForRestriction] [ReadOnly] public ComponentDataArray<Position> targetPositions;
+            [NativeDisableParallelForRestriction] [ReadOnly] public ComponentDataArray<Position> obstaclePositions;
             
-            void NearestPosition(NativeArray<Position> targets, float3 position, out float3 nearestPosition, out float nearestDistance )
+            void NearestPosition(ComponentDataArray<Position> targets, float3 position, out float3 nearestPosition, out float nearestDistance )
             {
                 nearestPosition = targets[0].Value;
                 nearestDistance = math.lengthSquared(position-nearestPosition);
@@ -112,57 +86,89 @@ namespace Samples.Boids
                 nearestDistance = math.sqrt(nearestDistance);
             }
             
+
+            public void Execute(int firstIndex, int index)
+            {
+                var cell = cells[firstIndex];
+                
+                cellIndices[index] = firstIndex;
+                
+                if (firstIndex == index)
+                {
+                    var position = cells[firstIndex].separation / -cells[firstIndex].count;
+                    
+                    float3 nearestObstaclePosition;
+                    float   nearestObstacleDistance;
+                    NearestPosition(obstaclePositions,position,out nearestObstaclePosition,out nearestObstacleDistance);
+
+                    cell.obstacleDistance = nearestObstacleDistance;
+                    cell.obstaclePosition = nearestObstaclePosition;
+                
+                    float3 nearestTargetPosition;
+                    float  nearestTargetDistance;
+                    NearestPosition(targetPositions,position,out nearestTargetPosition,out nearestTargetDistance);
+                    
+                    cell.targetPosition = nearestTargetPosition;
+                }
+                else
+                {
+                    cell.count      += 1.0f;
+                    cell.alignment  += cells[index].alignment;
+                    cell.separation += cells[index].separation;
+                }
+                
+                cells[firstIndex] = cell;
+            }
+        }
+
+        [ComputeJobOptimization]
+        struct Steer : IJobParallelFor
+        {
+            [ReadOnly] public NativeArray<int>         cellIndices;
+            [ReadOnly] public NativeArray<Cell>        cells;
+            [ReadOnly] public Boid                     settings;
+            public float                               dt;
+            public ComponentDataArray<Position>        positions;
+            public ComponentDataArray<Heading>         headings;
+            public ComponentDataArray<TransformMatrix> transformMatrices;
+            
             public void Execute(int index)
             {
-                var forward              = headings[index].Value;
-                var position             = positions[index].Value;
-                var cellIndex            = cellIndices[index];
-                var count                = cells[cellIndex].count;
-                var cellAlignment        = cells[cellIndex].alignment;
-                var cellSeparation       = cells[cellIndex].separation;
-                
-                float3 nearestObstaclePosition;
-                float nearestObstacleDistance;
-                NearestPosition(obstaclePositions,position,out nearestObstaclePosition,out nearestObstacleDistance);
-                
-                float3 nearestTargetPosition;
-                float nearestTargetDistance;
-                NearestPosition(targetPositions,position,out nearestTargetPosition,out nearestTargetDistance);
+                var forward                           = headings[index].Value;
+                var position                          = positions[index].Value;
+                var cellIndex                         = cellIndices[index];
+                var neighborCount                     = cells[cellIndex].count;
+                var cellAlignment                     = cells[cellIndex].alignment;
+                var cellSeparation                    = cells[cellIndex].separation;
+                var nearestObstacleDistance           = cells[cellIndex].obstacleDistance;
+                var nearestObstaclePosition           = cells[cellIndex].obstaclePosition;
+                var nearestTargetPosition             = cells[cellIndex].targetPosition;
                 
                 var obstacleSteering                  = (position - nearestObstaclePosition);
                 var avoidObstacleHeading              = (nearestObstaclePosition + math_experimental.normalizeSafe(obstacleSteering) * settings.obstacleAversionDistance)-position;
                 var targetHeading                     = settings.targetWeight * math_experimental.normalizeSafe(nearestTargetPosition - position);
                 var nearestObstacleDistanceFromRadius = nearestObstacleDistance - settings.obstacleAversionDistance;
                 
-                var alignmentResult      = alignmentWeight * math_experimental.normalizeSafe((cellAlignment/count)-forward);
-                var separationResult     = separationWeight * math_experimental.normalizeSafe((position * count) + cellSeparation);
-                var normalHeading        = math_experimental.normalizeSafe(alignmentResult + separationResult + targetHeading);
-                var targetForward        = math.select(normalHeading, avoidObstacleHeading, nearestObstacleDistanceFromRadius < 0);
-                var nextHeading          = math_experimental.normalizeSafe(forward + dt*(targetForward-forward));
-                var nextPosition         = position + (nextHeading * speed * dt);
-                var rottrans             = math.lookRotationToMatrix(nextPosition, nextHeading, math.up());
+                var alignmentResult                   = settings.alignmentWeight * math_experimental.normalizeSafe((cellAlignment/neighborCount)-forward);
+                var separationResult                  = settings.separationWeight * math_experimental.normalizeSafe((position * neighborCount) + cellSeparation);
+                var normalHeading                     = math_experimental.normalizeSafe(alignmentResult + separationResult + targetHeading);
+                var targetForward                     = math.select(normalHeading, avoidObstacleHeading, nearestObstacleDistanceFromRadius < 0);
+                var nextHeading                       = math_experimental.normalizeSafe(forward + dt*(targetForward-forward));
+                var nextPosition                      = position + (nextHeading * settings.speed * dt);
+                var rottrans                          = math.lookRotationToMatrix(nextPosition, nextHeading, math.up());
                 
-                headings[index]          = new Heading {Value = nextHeading};
-                positions[index]         = new Position {Value = nextPosition};
-                transformMatrices[index] = new TransformMatrix { Value = rottrans };
+                headings[index]                       = new Heading {Value = nextHeading};
+                positions[index]                      = new Position {Value = nextPosition};
+                transformMatrices[index]              = new TransformMatrix { Value = rottrans };
             }
         }
 
-        struct Dispose : IJob
-        {
-            [DeallocateOnJobCompletion] [ReadOnly] public NativeArray<Position> targetPositions;
-            [DeallocateOnJobCompletion] [ReadOnly] public NativeArray<Position> obstaclePositions;
-            public void Execute()
-            {
-            }
-        }
-        
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
             EntityManager.GetAllUniqueSharedComponentDatas(m_UniqueTypes);
             
-            var obstacleSourcePositions = m_ObstacleGroup.GetComponentDataArray<Position>();
-            var targetSourcePositions = m_TargetGroup.GetComponentDataArray<Position>();
+            var obstaclePositions = m_ObstacleGroup.GetComponentDataArray<Position>();
+            var targetPositions = m_TargetGroup.GetComponentDataArray<Position>();
             
             // Ingore typeIndex 0, can't use the default for anything meaningful.
             for (int typeIndex = 1; typeIndex < m_UniqueTypes.Count; typeIndex++)
@@ -175,8 +181,6 @@ namespace Samples.Boids
 
                 var cacheIndex = typeIndex - 1;
                 var boidCount = positions.Length;
-                var targetPositions = new NativeArray<Position>(targetSourcePositions.Length, Allocator.TempJob,NativeArrayOptions.UninitializedMemory);
-                var obstaclePositions = new NativeArray<Position>(obstacleSourcePositions.Length, Allocator.TempJob,NativeArrayOptions.UninitializedMemory);
                 var cells = new NativeArray<Cell>(boidCount, Allocator.TempJob,NativeArrayOptions.UninitializedMemory);
                 var cellIndices = new NativeArray<int>(boidCount, Allocator.TempJob,NativeArrayOptions.UninitializedMemory);
                 var hashMap = new NativeMultiHashMap<int,int>(boidCount,Allocator.TempJob);
@@ -215,50 +219,24 @@ namespace Samples.Boids
                 {
                     cells = cells,
                     cellIndices = cellIndices,
+                    targetPositions = targetPositions,
+                    obstaclePositions = obstaclePositions
                 };
                 var cellsJobHandle = cellsJob.Schedule(hashMap,64,hashPositionsJobHandle);
 
-                var targetPositionsJob = new CopyComponentData<Position>
-                {
-                    source = targetSourcePositions,
-                    results = targetPositions
-                };
-                var targetPositionsJobHandle = targetPositionsJob.Schedule(targetSourcePositions.Length,4,cellsJobHandle);
-                
-                var obstaclePositionsJob = new CopyComponentData<Position>
-                {
-                    source = obstacleSourcePositions,
-                    results = obstaclePositions
-                };
-                var obstaclePositionsJobHandle = obstaclePositionsJob.Schedule(obstacleSourcePositions.Length,4,cellsJobHandle);
-
-                var targetObstacleBarrierJobHandle = JobHandle.CombineDependencies(targetPositionsJobHandle, obstaclePositionsJobHandle);
-                
                 var steerJob = new Steer
                 {
                     cellIndices = nextCells.cellIndices,
                     cells = nextCells.cells,
-                    targetPositions = targetPositions,
-                    obstaclePositions = obstaclePositions,
                     settings = settings,
-                    dt = new float3(Time.deltaTime,Time.deltaTime,Time.deltaTime),
-                    alignmentWeight = new float3(settings.alignmentWeight,settings.alignmentWeight,settings.alignmentWeight),
-                    separationWeight = new float3(settings.separationWeight,settings.separationWeight,settings.separationWeight),
-                    speed = new float3(settings.speed,settings.speed,settings.speed),
+                    dt = Time.deltaTime,
                     positions = positions,
                     headings = headings,
                     transformMatrices = transformMatrices
                 };
-                var steerJobHandle = steerJob.Schedule(boidCount, 64, targetObstacleBarrierJobHandle);
+                var steerJobHandle = steerJob.Schedule(boidCount, 64, cellsJobHandle);
                     
-                var disposeJob = new Dispose
-                {
-                    targetPositions = targetPositions,
-                    obstaclePositions = obstaclePositions,
-                };
-                var disposeJobHandle = disposeJob.Schedule(steerJobHandle);
-
-                inputDeps = disposeJobHandle;
+                inputDeps = steerJobHandle;
             }
             m_UniqueTypes.Clear();
             

bc028ce3441c2e92846f8cd670cf7a342458464f
Daniel Brauer <danielbrauer@unity3d.com>
2018-03-28 14:38:50 +0200

    [editor] Cache ComponentGroup matches through frame
    Style improvements

--- Samples/Packages/com.unity.entities/Unity.Entities.Editor/EntityDebugger/EntitySelectionProxyEditor.cs
+++ Samples/Packages/com.unity.entities/Unity.Entities.Editor/EntityDebugger/EntitySelectionProxyEditor.cs
@@ -14,6 +14,8 @@ namespace Unity.Entities.Editor
     {
         private EntityIMGUIVisitor visitor;
 
+        private List<Tuple<ScriptBehaviourManager, List<ComponentGroup>>> cachedMatches = new List<Tuple<ScriptBehaviourManager, List<ComponentGroup>>>();
+
         [SerializeField] private bool showSystems;
         
         void OnEnable()
@@ -30,42 +32,66 @@ namespace Unity.Entities.Editor
             targetProxy.Container.PropertyBag.VisitStruct(ref container, visitor);
 
             GUI.enabled = true;
+            
+            ++EditorGUI.indentLevel;
+            GUILayout.BeginVertical(GUI.skin.box);
             showSystems = EditorGUILayout.Foldout(showSystems, "Used by Systems");
+            
             if (showSystems)
             {
-                using (var entityComponentTypes = targetProxy.Manager.GetComponentTypes(targetProxy.Entity, Allocator.Temp))
+                if (cachedMatches.Count == 0)
+                {
+                    CollectMatches(targetProxy);
+                }
+    
+                foreach (var pair in cachedMatches)
+                {
+                    var type = pair.Item1.GetType();
+                    GUILayout.Label(new GUIContent(type.Name, type.AssemblyQualifiedName));
+                    ++EditorGUI.indentLevel;
+                    foreach (var componentGroup in pair.Item2)
+                    {
+                        GUILayout.BeginHorizontal();
+                        if (GUILayout.Button(string.Join(", ", from x in componentGroup.Types.Skip(1) select x.Name)))
+                        {
+                            EntityDebugger.FocusSelectionInSystem(pair.Item1 as ComponentSystemBase, componentGroup);
+                        }
+                        GUILayout.FlexibleSpace();
+                        GUILayout.EndHorizontal();
+                    }
+
+                    --EditorGUI.indentLevel;
+                }
+
+                if (Event.current.type == EventType.Repaint)
+                {
+                    cachedMatches.Clear();
+                }
+            }
+            GUILayout.EndVertical();
+        }
+
+        private void CollectMatches(EntitySelectionProxy targetProxy)
+        {
+            using (var entityComponentTypes = targetProxy.Manager.GetComponentTypes(targetProxy.Entity, Allocator.Temp))
+            {
+                foreach (var manager in targetProxy.World.BehaviourManagers)
                 {
                     var componentGroupList = new List<ComponentGroup>();
-                    foreach (var manager in targetProxy.World.BehaviourManagers)
+                    var system = manager as ComponentSystemBase;
+                    if (system == null) continue;
+                    foreach (var componentGroup in system.ComponentGroups)
                     {
-                        componentGroupList.Clear();
-                        var system = manager as ComponentSystemBase;
-                        if (system == null) continue;
-                        foreach (var componentGroup in system.ComponentGroups)
-                        {
-                            if (Match(componentGroup, entityComponentTypes))
-                                componentGroupList.Add(componentGroup);
-                        }
-    
-                        if (componentGroupList.Count > 0)
-                        {
-                            GUILayout.Label(manager.GetType().ToString());
-                            ++EditorGUI.indentLevel;
-                            foreach (var componentGroup in componentGroupList)
-                            {
-                                if (GUILayout.Button(string.Join(", ", from x in componentGroup.Types.Skip(1) select x.Name)))
-                                {
-                                    EntityDebugger.FocusSelectionInSystem(system, componentGroup);
-                                }
-                            }
-    
-                            --EditorGUI.indentLevel;
-                        }
+                        if (Match(componentGroup, entityComponentTypes))
+                            componentGroupList.Add(componentGroup);
+                    }
+
+                    if (componentGroupList.Count > 0)
+                    {
+                        cachedMatches.Add(new Tuple<ScriptBehaviourManager, List<ComponentGroup>>(manager, componentGroupList));
                     }
                 }
             }
-            
-            
         }
 
         private static bool Match(ComponentGroup group, NativeArray<ComponentType> entityComponentTypes)

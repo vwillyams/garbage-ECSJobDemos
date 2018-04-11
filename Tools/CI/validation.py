253338420c574e0dbb33b45157c17dd7deff7376
Henrik Poulsen <henrikp@unity3d.com>
2018-04-11 20:15:23 +0200

    Made validation a bit dumber regarding getting the revisions, so it only gets the revision and nothing else and saves that to the unity_revision.txt. If it finds multiple revisions in the results it raises and exception so it won't be bad by accident

--- Tools/CI/validation.py
+++ Tools/CI/validation.py
@@ -5,7 +5,6 @@ import glob
 import json
 
 file_blacklist = ['.DS_Store']
-OSes_available = ['macOS', 'windows', 'linux']
 artifactory_url = "https://artifactory.eu-cph-1.unityops.net/"
 artifactory_repository = "core-automation"
 args = None
@@ -16,29 +15,39 @@ def get_url_json(url):
     response = urllib2.urlopen(url)
     return json.loads(response.read())
 
-def artifactory_search(type):
+def artifactory_search_for_revision(type):
     # If the build_version contains a slash we treat it as a branch instead of as a version and will use a custom
     # branch build instead
-
     artifactory_result = {type:{}}
 
+    # First we get all the artifacts that match our query
     if '/' not in args.build_version and not args.build_version.startswith("trunk"):
-        for os in OSes_available:
-            artifact_uri = get_url_json("{0}/{1}".format(artifactory_url,
-                                                        "api/search/prop?build={1}&os={2}&type={3}&custom=False&repos={0}".format(
-                                                        artifactory_repository, args.build_version, os, type)))['results']
-            if artifact_uri:
-                artifactory_result[type][os]=artifact_uri[0]['uri']
+        artifact_uri = get_url_json("{0}/{1}".format(artifactory_url,
+                                                    "api/search/prop?build={1}&type={2}&custom=False&repos={0}".format(
+                                                    artifactory_repository, args.build_version, type)))['results']
     else:
-        for os in OSes_available:
-            artifact_uri = get_url_json("{0}/{1}".format(artifactory_url,
-                                                         "api/search/prop?build=&os={0}&type={1}&custom=True&branch={2}&repos={3}".format(
-                                                         os, type, args.build_version, artifactory_repository)))['results']
+        artifact_uri = get_url_json("{0}/{1}".format(artifactory_url,
+                                                     "api/search/prop?build=&type={0}&custom=True&branch={1}&repos={2}".format(type, args.build_version, artifactory_repository)))['results']
 
-            if artifact_uri:
-                artifactory_result[type][os] = artifact_uri[0]['uri']
+    if len(artifact_uri) == 0:
+        raise Exception("Found no artifactory artifacts matching {0}.".format(args.build_version))
+    revision = None
+    # Here we do a new api request which requests the revision property. Sadly this has to be two separte api calls
+    for artifact in artifact_uri:
+        # /api/storage/libs-release-local/org/acme?properties\[=x[,y]\]
+        revision_result = get_url_json("{0}?properties=revision".format(artifact['uri']))
+        if revision is None:
+            revision = revision_result['properties']['revision'][0]
+            continue
+        # Just some sanity checking so we don't get mismatching revisions here. There should only be one revision
+        if revision == revision_result['properties']['revision'][0]:
+            continue
+        raise Exception("Found multiple revisions on artifactory that matches {0}. The revisions are {1} and {2}".format(args.build_version, revision, revision_result['properties']['revision'][0]))
 
-    return artifactory_result
+    if not revision:
+        raise Exception("Could not find any revision for {0}".format(args.build_version))
+
+    return revision
 
 def get_packages_folder(project_path):
     if os.path.isfile(os.path.join(project_path, "Packages/manifest.json")):
@@ -62,24 +71,14 @@ def check_metafiles_in_package(package_folder):
             return False
     return True
 
-def generate_unity_revisions_file(editor, mono, il2cpp):
-    json_data = {}
-    for key in editor:
-        json_data[key] = editor[key]
-    for key in mono:
-        json_data[key] = mono[key]
-    for key in il2cpp:
-        json_data[key] = il2cpp[key]
-
+def generate_unity_revisions_file(revision):
     with open('unity_revision.txt', 'wb') as f:
-        json.dump(json_data, f, indent = 4)
+        f.write(revision)
 
 def main():
-    editor_artifacts = artifactory_search("editor")
-    standalone_artifacts_mono = artifactory_search("standalone-mono")
-    standalone_artifacts_il2cpp = artifactory_search("standalone-il2cpp")
+    revision = artifactory_search_for_revision("editor")
 
-    generate_unity_revisions_file(editor_artifacts, standalone_artifacts_mono, standalone_artifacts_il2cpp)
+    generate_unity_revisions_file(revision)
 
     for project_path in args.project_path:
         packages_folder = get_packages_folder(project_path[0])

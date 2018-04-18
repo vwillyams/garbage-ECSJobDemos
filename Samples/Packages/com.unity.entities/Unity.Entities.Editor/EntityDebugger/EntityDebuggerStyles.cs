9a3da6a62ec8294e2968680bb2818ee2cc80ea2d
Daniel Brauer <danielbrauer@unity3d.com>
2018-04-18 17:36:58 +0200

    [editor] Namespaces

--- Samples/Packages/com.unity.entities/Unity.Entities.Editor/EntityDebugger/EntityDebuggerStyles.cs
+++ Samples/Packages/com.unity.entities/Unity.Entities.Editor/EntityDebugger/EntityDebuggerStyles.cs
@@ -3,24 +3,28 @@
 using UnityEditor;
 using UnityEngine;
 
-public static class EntityDebuggerStyles
+namespace Unity.Entities.Editor
 {
-
-    public static GUIStyle ComponentRequired => StyleAsset.styles[0];
-    public static GUIStyle ComponentSubtractive => StyleAsset.styles[1];
-
-    private static GUIStyleAsset StyleAsset
+    public static class EntityDebuggerStyles
     {
-        get
-        {
-            if (!styleAsset)
-            {
-                styleAsset = AssetDatabase.LoadAssetAtPath<GUIStyleAsset>("Packages/com.unity.entities/Unity.Entities.Editor/Resources/EntityDebuggerStyles.asset");
-            }
 
-            return styleAsset;
+        public static GUIStyle ComponentRequired => StyleAsset.styles[0];
+        public static GUIStyle ComponentSubtractive => StyleAsset.styles[1];
+
+        private static GUIStyleAsset StyleAsset
+        {
+            get
+            {
+                if (!styleAsset)
+                {
+                    styleAsset = AssetDatabase.LoadAssetAtPath<GUIStyleAsset>(
+                        "Packages/com.unity.entities/Unity.Entities.Editor/Resources/EntityDebuggerStyles.asset");
+                }
+
+                return styleAsset;
+            }
         }
+
+        private static GUIStyleAsset styleAsset;
     }
-    
-    private static GUIStyleAsset styleAsset;
 }
--- Samples/Packages/com.unity.entities/Unity.Entities.Editor/GUIStyleAsset.cs
+++ Samples/Packages/com.unity.entities/Unity.Entities.Editor/GUIStyleAsset.cs
@@ -1,10 +1,11 @@
-﻿
+﻿using UnityEngine;
 
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Styles.asset", menuName = "GUI Style Asset", order = 600)]
-public class GUIStyleAsset : ScriptableObject
+namespace Unity.Entities.Editor
 {
+    [CreateAssetMenu(fileName = "Styles.asset", menuName = "GUI Style Asset", order = 600)]
+    public class GUIStyleAsset : ScriptableObject
+    {
 
-    public GUIStyle[] styles;
+        public GUIStyle[] styles;
+    }
 }

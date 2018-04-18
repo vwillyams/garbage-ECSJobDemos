45f4663db09f1ce96ce76a9336a4f81091c756a8
Henrik Poulsen <henrikp@unity3d.com>
2018-04-18 15:35:34 +0200

    Added a scene switcher scene that loads in a different scene every X seconds which we can hopefully use in CI to reduce the build times by an hour or more

---
+++ Samples/Assets/GameCode/SceneSwitcher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public float SceneSwitchInterval = 5.0f;
+
+    public float TimeUntilNextSwitch = 0.0f;
+
+    public int CurrentSceneIndex = -1;
+	// Use this for initialization
+	void Start ()
+	{
+	    DontDestroyOnLoad(this);
+	    LoadNextScene();
+	}
+
+    private void LoadNextScene()
+    {
+        var sceneCount = SceneManager.sceneCountInBuildSettings;
+        var nextIndex = CurrentSceneIndex + 1;
+        if (nextIndex >= sceneCount)
+            nextIndex = 0;
+        if (SceneManager.GetSceneByBuildIndex(nextIndex).name == "SceneSwitcher")
+            nextIndex++;
+
+        Debug.Log(sceneCount);
+        Debug.Log("Changing to scene: " + nextIndex);
+        bool firstTime = CurrentSceneIndex == -1;
+        TimeUntilNextSwitch = SceneSwitchInterval;
+        CurrentSceneIndex = nextIndex;
+
+        if (!firstTime)
+        {
+            World.Active.Dispose();
+            DefaultWorldInitialization.Initialize(SceneManager.GetSceneByBuildIndex(nextIndex).name, false);
+        }
+        
+        SceneManager.LoadScene(nextIndex);
+        
+        
+    }
+
+    // Update is called once per frame
+	void Update ()
+	{
+	    TimeUntilNextSwitch -= Time.deltaTime;
+	    if (TimeUntilNextSwitch > 0.0f)
+	        return;
+	    
+	    LoadNextScene();
+	}
+}
